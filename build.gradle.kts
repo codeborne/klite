148547a42d7c66c04b2725d93c55833c402c4d5a
Anton Keks <anton@codeborne.com>
2021-11-28 14:35:15 +0200

    use simple gradle project structure of src/test

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,17 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.5.2")
 }
 
+sourceSets {
+  named("main") {
+    java.srcDirs("src")
+    resources.srcDirs("src").exclude("**/*.kt")
+  }
+  named("test") {
+    java.srcDirs("test")
+    resources.srcDirs("test").exclude("**/*.kt")
+  }
+}
+
 tasks.test {
   useJUnit()
 }
