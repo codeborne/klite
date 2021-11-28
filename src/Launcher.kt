05742e41fd01ab7871c37242dcf810acdd125503
Anton Keks <anton@codeborne.com>
2021-11-28 15:34:28 +0200

    add delay handler to test coroutines

--- src/Launcher.kt
+++ src/Launcher.kt
@@ -1,8 +1,13 @@
+import kotlinx.coroutines.delay
 import server.Server
 
 fun main() {
   Server(8080).apply {
     route("/") { "Hello World" }
+    route("/delay") {
+      delay(1000)
+      "Waited for 1 sec"
+    }
     start()
   }
 }
