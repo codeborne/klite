fe7ff61ce4aabeec01794d5a162ba9b886d1c784
Anton Keks <anton@codeborne.com>
2021-11-29 00:16:53 +0200

    pass HttpExchange to controller method if requested

--- src/server/Routes.kt
+++ src/server/Routes.kt
@@ -1,10 +1,15 @@
 package server
 
+import com.sun.net.httpserver.HttpExchange
 import java.lang.reflect.Method
 
 annotation class Path(val value: String)
 annotation class GET(val value: String = "")
 
 fun toHandler(instance: Any, method: Method): Handler {
-  return { exchange -> method.invoke(instance) }
+  val params = method.parameters
+  return { exchange -> method.invoke(instance, *params.map { p ->
+    if (p.type == HttpExchange::class.java) exchange
+    else null
+  }.toTypedArray())}
 }
