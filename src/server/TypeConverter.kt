6679bbf592033be89371784d93767dfefee594d2
Anton Keks <anton@codeborne.com>
2021-12-02 14:22:47 +0200

    implement TypeConverter for annotation-based routing

---
+++ src/server/TypeConverter.kt
@@ -0,0 +1,44 @@
+package server
+
+import java.lang.reflect.InvocationTargetException
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.reflect.KClass
+
+private typealias Creator<T> = (s: String) -> T?
+
+open class TypeConverter(moreCreators: Map<KClass<*>, Creator<*>> = emptyMap()) {
+  private val creatorCache = ConcurrentHashMap(mapOf(
+    UUID::class to UUID::fromString
+  ) + moreCreators)
+
+  inline fun <reified T: Any> fromString(s: String) = fromString(s, T::class)
+
+  fun <T: Any> fromString(s: String, type: KClass<T>): T? =
+    (creatorCache[type] as? Creator<T> ?: findCreator(type).also { creatorCache[type] = it }).invoke(s)
+
+  private fun <T: Any> findCreator(type: KClass<T>): Creator<T> =
+    try { constructorCreator(type) }
+    catch (e: NoSuchMethodException) {
+      try { parseMethodCreator(type) }
+      catch (e2: NoSuchMethodException) {
+        error("Cannot create instances of $type:\n$e\n$e2")
+      }
+    }
+
+  private fun <T: Any> constructorCreator(type: KClass<T>): Creator<T> {
+    val constructor = type.java.getConstructor(String::class.java)
+    return { s: String ->
+      try { constructor.newInstance(s) }
+      catch (e: InvocationTargetException) { throw e.targetException }
+    }
+  }
+
+  private fun <T: Any> parseMethodCreator(type: KClass<T>): Creator<T> {
+    val parse = type.java.getMethod("parse", CharSequence::class.java)
+    return { s: String ->
+      try { parse.invoke(null, s) as T }
+      catch (e: InvocationTargetException) { throw e.targetException }
+    }
+  }
+}
---
+++ test/server/TypeConverterTest.kt
@@ -0,0 +1,36 @@
+package server
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.net.URI
+import java.time.LocalDate
+import java.time.Period
+import java.util.*
+
+class TypeConverterTest {
+  val converter = TypeConverter()
+
+  @Test
+  fun `pre-defined`() {
+    assertThat(converter.fromString<UUID>("05e1cebe-67dc-4780-b5df-472edd55fab6")).isEqualTo(UUID.fromString("05e1cebe-67dc-4780-b5df-472edd55fab6"))
+  }
+
+  @Test
+  fun constructor() {
+    assertThat(converter.fromString<URI>("http://hello/")).isEqualTo(URI("http://hello/"))
+  }
+
+  @Test
+  fun parse() {
+    assertThat(converter.fromString<LocalDate>("2021-10-21")).isEqualTo(LocalDate.parse("2021-10-21"))
+    assertThat(converter.fromString<Period>("P1D")).isEqualTo(Period.parse("P1D"))
+  }
+
+  @Test
+  fun `no creator`() {
+    assertThrows<IllegalStateException> {
+      converter.fromString<TypeConverter>("some string")
+    }
+  }
+}
