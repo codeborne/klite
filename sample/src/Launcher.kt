53b6abc50347fb71ca499a260ffd8d212b11d014
Anton Keks <anton@codeborne.com>
2021-12-02 22:27:47 +0200

    cannot use illegal-access=permit with Java 17 anymore

--- sample/src/Launcher.kt
+++ sample/src/Launcher.kt
@@ -4,7 +4,7 @@ import server.Server
 import server.annotations.annotated
 import java.nio.file.Path
 
-// run with --illegal-access=permit to allow accessing Java built-in Mime types
+// run with --add-exports java.base/sun.net.www=ALL-UNNAMED to allow accessing Java built-in Mime types
 fun main() {
   System.setProperty("java.util.logging.config.file", "logging.properties")
 
