f3264d0a79c6b04c475aadae3e5fd01ace166a96
Anton Keks <anton@codeborne.com>
2021-12-02 22:49:06 +0200

    add logging/checks to AssetsHandler

--- core/src/server/AssetsHandler.kt
+++ core/src/server/AssetsHandler.kt
@@ -3,29 +3,42 @@ package server
 
 import sun.net.www.MimeTable
 import java.io.IOException
+import java.lang.System.Logger.Level.WARNING
 import java.nio.file.Path
 import java.time.ZoneOffset.UTC
 import java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME
-import kotlin.io.path.div
-import kotlin.io.path.getLastModifiedTime
-import kotlin.io.path.name
-import kotlin.io.path.readBytes
+import kotlin.io.path.*
 
-class AssetsHandler(val path: Path, val indexFile: String = "index.html", val cacheControl: String = "max-age=86400"): Handler {
+class AssetsHandler(
+  val path: Path,
+  val indexFile: String = "index.html",
+  val cacheControl: String = "max-age=86400"
+): Handler {
   private val mimeTypes = MimeTable.getDefaultTable()
+  private val logger = System.getLogger(javaClass.name)
+
+  init {
+    if (!path.isDirectory()) logger.log(WARNING, "Assets path ${path.toAbsolutePath()} is not a readable directory")
+  }
 
   override suspend fun invoke(exchange: HttpExchange) {
     // TODO AsynchronousFileChannel.open(path.resolve(exchange.requestPath), READ).read().await()
     try {
-      val file = path / exchange.path.substring(1)
-      if (!file.startsWith(path)) return exchange.send(403, file)
-      val lastModified = RFC_1123_DATE_TIME.format(file.getLastModifiedTime().toInstant().atOffset(UTC))
-      if (lastModified == exchange.header("if-modified-since")) return exchange.send(304, null)
-      exchange.header("Last-Modified", lastModified)
-      exchange.header("Cache-Control", cacheControl)
-      exchange.send(200, file.readBytes(), mimeTypes.getContentTypeFor(file.name))
+      var file = path / exchange.path.substring(1)
+      if (!file.startsWith(path)) return exchange.send(403, exchange.path)
+      if (file.isDirectory()) file /= indexFile
+      if (!file.exists()) return exchange.send(404, exchange.path)
+      send(file, exchange)
     } catch (e: IOException) {
       exchange.send(404, e.message)
     }
+  }
+
+  private fun send(file: Path, exchange: HttpExchange) {
+    val lastModified = RFC_1123_DATE_TIME.format(file.getLastModifiedTime().toInstant().atOffset(UTC))
+    if (lastModified == exchange.header("If-Modified-Since")) return exchange.send(304, null)
+    exchange.header("Last-Modified", lastModified)
+    exchange.header("Cache-Control", cacheControl)
+    exchange.send(200, file.readBytes(), mimeTypes.getContentTypeFor(file.name))
   }
 }
