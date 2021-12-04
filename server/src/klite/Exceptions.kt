b255828d12d51ae39b5a292369fe09401cfb6585
Anton Keks <anton@codeborne.com>
2021-12-04 22:30:03 +0200

    disable filling of stack trace for StatusCodeExceptions - for performance

--- server/src/klite/Exceptions.kt
+++ server/src/klite/Exceptions.kt
@@ -1,9 +1,9 @@
 package klite
 
-import java.lang.System.Logger.Level.ERROR
-import kotlin.reflect.KClass
+open class StatusCodeException(val statusCode: Int, content: String?): Exception(content) {
+  override fun fillInStackTrace() = this
+}
 
-open class StatusCodeException(val statusCode: Int, content: String?): Exception(content)
 class NotFoundException(content: String?): StatusCodeException(404, content)
 class ForbiddenException(content: String?): StatusCodeException(403, content)
 class UnauthorizedException(content: String?): StatusCodeException(401, content)
