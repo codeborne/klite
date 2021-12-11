1240fa32053c4bed818a7a3cc8b2660b9ca6ea38
Anton Keks <anton@codeborne.com>
2021-12-11 17:27:47 +0200

    set thread name to request id so that all logs from the request would be identifiable

--- server/src/klite/RequestLogger.kt
+++ server/src/klite/RequestLogger.kt
@@ -1,18 +1,41 @@
 package klite
 
+import kotlinx.coroutines.ThreadContextElement
+import kotlinx.coroutines.withContext
+import java.util.concurrent.atomic.AtomicLong
+import kotlin.coroutines.AbstractCoroutineContextElement
+import kotlin.coroutines.CoroutineContext
+
 typealias RequestLogFormatter = HttpExchange.(ms: Long) -> String
 
 open class RequestLogger(
   val formatter: RequestLogFormatter = { ms -> "$remoteAddress $method $path$query: $statusCode in $ms ms" }
-): Before {
+): Decorator {
   private val logger = logger()
+  private val prefix = (0xFFFF * Math.random()).toInt().toString(16)
+  private val counter = AtomicLong()
 
-  override suspend fun before(exchange: HttpExchange) {
+  init {
+    logger.info("Starting node: $prefix")
+  }
+
+  override suspend fun invoke(exchange: HttpExchange, handler: Handler): Any? {
     val start = System.nanoTime()
-    // TODO: unique request id from X-Request-Id or generated
+    val requestId = "$prefix-${counter.incrementAndGet()}"
+    // TODO: use X-Request-Id header if available
     exchange.onComplete {
       val ms = (System.nanoTime() - start) / 1000_000
       logger.info(formatter(exchange, ms))
     }
+    return withContext(RequestThreadNameContext(requestId)) {
+      handler(exchange)
+    }
   }
+}
+
+class RequestThreadNameContext(private val requestId: String): ThreadContextElement<String?>, AbstractCoroutineContextElement(Key) {
+  companion object Key: CoroutineContext.Key<RequestThreadNameContext>
+
+  override fun updateThreadContext(context: CoroutineContext) = Thread.currentThread().also { it.name = requestId }.name
+  override fun restoreThreadContext(context: CoroutineContext, oldState: String?) { Thread.currentThread().name = oldState }
 }
