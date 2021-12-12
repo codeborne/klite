d936029ba73fb330a4de035ded91e2821353765c
Anton Keks <anton@codeborne.com>
2021-12-12 19:02:50 +0200

    check readme links and fail the build if something is obsolete

---
+++ .github/workflows/check-readme-links.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for f in `find -name 'README.md'`; do
+  echo "Checking $f"
+  PATHS=`grep -oP "(?<=\]\()[^)]*(?=\))" $f | grep -v '^http'`
+  for l in $PATHS; do
+    path=`dirname $f`/$l
+    if [ ! -e "$path" ]; then
+      echo "$path doesn't exist" && exit 1
+    fi
+  done
+done
