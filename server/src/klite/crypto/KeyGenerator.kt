b005a0624e54abb4126b1ab200ca9b813d6120f6
Anton Keks <anton@codeborne.com>
2021-12-14 00:21:20 +0200

    create a new SecretKeySpec after generation with correct algorithm

--- server/src/klite/crypto/KeyGenerator.kt
+++ server/src/klite/crypto/KeyGenerator.kt
@@ -3,11 +3,12 @@ package klite.crypto
 import javax.crypto.SecretKey
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
+import javax.crypto.spec.SecretKeySpec
 
 class KeyGenerator(val keyFactory: SecretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256")) {
   fun keyFromSecret(password: String, salt: String? = "SaltyKlite"): SecretKey {
     val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256")
     val spec = PBEKeySpec(password.toCharArray(), salt?.toByteArray(), 65536, 256)
-    return factory.generateSecret(spec)
+    return SecretKeySpec(factory.generateSecret(spec).encoded, "AES")
   }
 }
