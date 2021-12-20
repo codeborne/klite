3e3128f55bd7e89ec62927db5bee957ebeedf999
Anton Keks <anton@codeborne.com>
2021-12-20 18:19:23 +0200

    skip loading .env file if it doesn't exist

--- server/src/klite/Config.kt
+++ server/src/klite/Config.kt
@@ -7,9 +7,12 @@ object Config {
   fun optional(env: String, default: String) = optional(env) ?: default
   fun required(env: String) = optional(env) ?: error("$env should be provided as system property or env var")
 
-  fun fromEnvFile(file: File = File(".env"), force: Boolean = false) = file.forEachLine {
-    it.split('=', limit = 2).let { (key, value) ->
-      if (force || optional(key) == null) System.setProperty(key, value)
+  fun fromEnvFile(file: File = File(".env"), force: Boolean = false) {
+    if (!force && !file.exists()) return logger().info("No .env file found")
+    file.forEachLine {
+      it.split('=', limit = 2).let { (key, value) ->
+        if (force || optional(key) == null) System.setProperty(key, value)
+      }
     }
   }
 }
