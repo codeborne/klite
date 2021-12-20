7e69adaac334185c61d3275fb841dacd7c8fce10
Anton Keks <anton@codeborne.com>
2021-12-20 18:22:33 +0200

    improve logging

--- server/src/klite/Config.kt
+++ server/src/klite/Config.kt
@@ -8,7 +8,7 @@ object Config {
   fun required(env: String) = optional(env) ?: error("$env should be provided as system property or env var")
 
   fun useEnvFile(file: File = File(".env"), force: Boolean = false) {
-    if (!force && !file.exists()) return logger().info("No .env file found")
+    if (!force && !file.exists()) return logger().info("No ${file.absolutePath} found, skipping")
     file.forEachLine {
       it.split('=', limit = 2).let { (key, value) ->
         if (force || optional(key) == null) System.setProperty(key, value)
