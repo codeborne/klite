bdddaf8cdbf2360c861bf44237120c08bba5ecfa
Anton Keks <anton@codeborne.com>
2021-12-20 18:17:38 +0200

    do not override env values with .env file by default

--- server/src/klite/Config.kt
+++ server/src/klite/Config.kt
@@ -7,7 +7,9 @@ object Config {
   fun optional(env: String, default: String) = optional(env) ?: default
   fun required(env: String) = optional(env) ?: error("$env should be provided as system property or env var")
 
-  fun fromEnvFile(file: File = File(".env")) = file.forEachLine {
-    it.split('=', limit = 2).let { System.setProperty(it[0], it[1]) }
+  fun fromEnvFile(file: File = File(".env"), force: Boolean = false) = file.forEachLine {
+    it.split('=', limit = 2).let { (key, value) ->
+      if (force || optional(key) == null) System.setProperty(key, value)
+    }
   }
 }
