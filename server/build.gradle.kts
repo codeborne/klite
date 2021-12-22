469ca41ac3a900d355832ed7249ce7f8f9ad2182
Anton Keks <anton@codeborne.com>
2021-12-22 22:11:35 +0200

    upgrade kotlin coroutines

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,4 +1,4 @@
 dependencies {
   api("org.jetbrains.kotlin:kotlin-reflect")
-  api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.5.2")
+  api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.0")
 }
