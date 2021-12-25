5797a04c7ac8654dc1c4fc3a95878cba6838eb79
Anton Keks <anton@codeborne.com>
2021-12-25 15:02:06 +0200

    try to add serializer for ErrorResponse (seems that contextual serializer is not used by default for types)

--- serialization/src/JsonBody.kt
+++ serialization/src/JsonBody.kt
@@ -1,6 +1,11 @@
 package klite.serialization
 
 import klite.*
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
 import kotlinx.serialization.json.encodeToStream
@@ -16,11 +21,24 @@ class JsonBody(
   override val contentType: String = "application/json"
 ): BodyParser, BodyRenderer, Extension {
   override fun <T: Any> parse(input: InputStream, type: KClass<T>): T = json.decodeFromStream(serializer(type.java), input) as T
-  override fun render(output: OutputStream, value: Any?) = json.encodeToStream(value, output)
+  override fun render(output: OutputStream, value: Any?) = when (value) {
+    is ErrorResponse -> json.encodeToStream(ErrorResponseSerializer, value, output)
+    else -> json.encodeToStream(serializer(value!!.javaClass), value, output)
+  }
 
   override fun install(server: Server) = server.run {
     registry.register(json)
     renderers += this@JsonBody
     parsers += this@JsonBody
   }
+}
+
+object ErrorResponseSerializer: KSerializer<ErrorResponse> {
+  private val serializer = SerializableErrorResponse.serializer()
+  override val descriptor = SerialDescriptor("ErrorResponse", serializer.descriptor)
+  override fun serialize(encoder: Encoder, value: ErrorResponse) =
+    encoder.encodeSerializableValue(serializer, SerializableErrorResponse(value.statusCode.value, value.reason, value.message))
+  override fun deserialize(decoder: Decoder): ErrorResponse = error("No need")
+
+  @Serializable class SerializableErrorResponse(val statusCode: Int, val reason: String, val message: String?)
 }
--- serialization/test/JsonBodyTest.kt
+++ serialization/test/JsonBodyTest.kt
@@ -1,14 +1,25 @@
 package klite.serialization
 
+import klite.ErrorResponse
+import klite.StatusCode
 import kotlinx.datetime.LocalDate
 import kotlinx.serialization.Serializable
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
 
 class JsonBodyTest {
+  val jsonBody = JsonBody()
+
   @Test fun `can create data classes`() {
-    val someData = JsonBody().parse("""{"email":"a@b.ee","date":"2021-12-12","extra":123}""".byteInputStream(), SomeData::class)
+    val someData = jsonBody.parse("""{"email":"a@b.ee","date":"2021-12-12","extra":123}""".byteInputStream(), SomeData::class)
     assertThat(someData).isEqualTo(SomeData(Email("a@b.ee"), LocalDate.parse("2021-12-12")))
+  }
+
+  @Test fun `can serialize ErrorResponse`() {
+    val out = ByteArrayOutputStream()
+    jsonBody.render(out, ErrorResponse(StatusCode.NotFound, "/"))
+    assertThat(out.toByteArray().decodeToString()).isEqualTo("{\"statusCode\":404,\"reason\":\"Not Found\",\"message\":\"/\"}")
   }
 }
 
