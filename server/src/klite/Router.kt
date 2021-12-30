716df1392bb8529564a7f8bc70cce99ff62eaf6f
Anton Keks <anton@codeborne.com>
2021-12-30 13:58:37 +0200

    return correct type of annotation

--- server/src/klite/Router.kt
+++ server/src/klite/Router.kt
@@ -76,7 +76,8 @@ enum class RequestMethod {
 }
 
 data class Route(val method: RequestMethod, val path: Regex, val handler: Handler, val annotations: List<Annotation> = emptyList()) {
-  fun <T: Annotation> annotation(key: KClass<T>) = annotations.find { key.javaObjectType.isAssignableFrom(it.javaClass) }
+  @Suppress("UNCHECKED_CAST")
+  fun <T: Annotation> annotation(key: KClass<T>): T? = annotations.find { key.javaObjectType.isAssignableFrom(it.javaClass) } as? T
   inline fun <reified T: Annotation> annotation() = annotation(T::class)
   inline fun <reified T: Annotation> hasAnnotation() = annotation(T::class) != null
 }
