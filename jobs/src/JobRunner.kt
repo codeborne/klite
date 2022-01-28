98dbe8cdffc535a05d58cc47e191f115b22a51f9
Anton Keks <anton@codeborne.com>
2022-01-28 14:24:55 +0200

    add some tests for JobRunner and make executor more easily redefineable

--- jobs/src/JobRunner.kt
+++ jobs/src/JobRunner.kt
@@ -10,6 +10,7 @@ import java.time.LocalDateTime
 import java.time.LocalTime
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeUnit.MINUTES
 import java.util.concurrent.TimeUnit.SECONDS
@@ -20,10 +21,12 @@ fun interface Job {
   suspend fun run()
 }
 
-class JobRunner(private val db: DataSource): Extension, CoroutineScope {
-  private val logger = logger()
-  private val executor = Executors.newScheduledThreadPool(Config.optional("JOB_RUNNER_THREADS", "3").toInt())
+class JobRunner(
+  private val db: DataSource,
+  private val executor: ScheduledExecutorService = Executors.newScheduledThreadPool(Config.optional("JOB_RUNNER_THREADS", "3").toInt())
+): Extension, CoroutineScope {
   override val coroutineContext = executor.asCoroutineDispatcher()
+  private val logger = logger()
   private val seq = AtomicLong()
   private val runningJobs = ConcurrentHashMap.newKeySet<kotlinx.coroutines.Job>()
 
@@ -31,25 +34,27 @@ class JobRunner(private val db: DataSource): Extension, CoroutineScope {
     server.onStop(::gracefulStop)
   }
 
+  internal fun runInTransaction(jobName: String, job: Job) {
+    val threadName = RequestThreadNameContext("${RequestLogger.prefix}/$jobName#${seq.incrementAndGet()}")
+    val tx = Transaction(db)
+    val launched = launch(start = UNDISPATCHED, context = TransactionContext(tx) + threadName) {
+      try {
+        job.run()
+        tx.close(true)
+      } catch (e: Exception) {
+        logger.error("$jobName failed", e)
+        tx.close(false)
+      }
+    }
+    runningJobs += launched
+    launched.invokeOnCompletion { runningJobs -= launched }
+  }
+
   fun schedule(job: Job, delay: Long, period: Long, unit: TimeUnit) {
-    val jobName = job::class.simpleName
+    val jobName = job::class.simpleName!!
     val startAt = LocalDateTime.now().plus(delay, unit.toChronoUnit())
     logger.info("$jobName will start at $startAt and run every $period $unit")
-    executor.scheduleAtFixedRate({
-      val threadName = RequestThreadNameContext("${RequestLogger.prefix}/$jobName#${seq.incrementAndGet()}")
-      val tx = Transaction(db)
-      val launched = launch(start = UNDISPATCHED, context = TransactionContext(tx) + threadName) {
-        try {
-          job.run()
-          tx.close(true)
-        } catch (e: Exception) {
-          logger.error("$jobName failed", e)
-          tx.close(false)
-        }
-      }
-      runningJobs += launched
-      launched.invokeOnCompletion { runningJobs -= launched }
-    }, delay, period, unit)
+    executor.scheduleAtFixedRate({ runInTransaction(jobName, job) }, delay, period, unit)
   }
 
   fun scheduleDaily(job: Job, delayMinutes: Long = (Math.random() * 10).toLong()) =
---
+++ jobs/test/JobRunnerTest.kt
@@ -0,0 +1,50 @@
+import io.mockk.mockk
+import io.mockk.verify
+import klite.Server
+import klite.jdbc.Transaction
+import klite.jobs.Job
+import klite.jobs.JobRunner
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.LocalTime
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit.MINUTES
+import java.util.concurrent.TimeUnit.SECONDS
+import javax.sql.DataSource
+
+class JobRunnerTest {
+  val db = mockk<DataSource>()
+  val executor = mockk<ScheduledExecutorService>(relaxed = true)
+  val runner = JobRunner(db, executor)
+
+  @Test fun `adds shutdown handler`() {
+    val server = mockk<Server>(relaxed = true)
+    runner.install(server)
+    verify { server.onStop(any()) }
+  }
+
+  @Test fun runInTransaction() {
+    val job = Job {
+      assertThat(Transaction.current()).isNotNull
+    }
+    runner.runInTransaction("My job", job)
+  }
+
+  @Test fun schedule() {
+    val job = mockk<Job>()
+    runner.schedule(job, 10, 20, SECONDS)
+    verify { executor.scheduleAtFixedRate(any(), 10, 20, SECONDS) }
+  }
+
+  @Test fun `scheduleDaily with delay`() {
+    val job = mockk<Job>()
+    runner.scheduleDaily(job, delayMinutes = 10)
+    verify { executor.scheduleAtFixedRate(any(), 10, 24 * 60, MINUTES) }
+  }
+
+  @Test fun `scheduleDaily at time`() {
+    val job = mockk<Job>()
+    runner.scheduleDaily(job, at = LocalTime.of(6, 30))
+    verify { executor.scheduleAtFixedRate(any(), match { it > 0 }, 24 * 60, MINUTES) }
+  }
+}
