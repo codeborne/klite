4ddff842b5cf8a0244716acd1b0a982c7b5de16f
Anton Keks <anton@codeborne.com>
2022-01-31 16:27:56 +0200

    rename argument to "provided" for more clarity

--- jdbc/src/BaseModel.kt
+++ jdbc/src/BaseModel.kt
@@ -12,9 +12,9 @@ interface BaseModel {
   val id: UUID
 }
 
-inline fun <reified T: Any> T.toValues(vararg values: Pair<KProperty1<T, *>, Any?>): Map<String, Any?> {
-  val provided = mapOf(*values)
-  return toValuesSkipping(provided.keys) + provided.mapKeys { it.key.name }
+inline fun <reified T: Any> T.toValues(vararg provided: Pair<KProperty1<T, *>, Any?>): Map<String, Any?> {
+  val values = mapOf(*provided)
+  return toValuesSkipping(values.keys) + values.mapKeys { it.key.name }
 }
 
 inline fun <reified T: Any> T.toValuesSkipping(vararg skip: KProperty1<T, *>) = toValuesSkipping(setOf(*skip))
@@ -25,10 +25,10 @@ inline fun <reified T: Any> T.toValuesSkipping(skip: Set<KProperty1<T, *>>): Map
 fun <T: Any> T.toValues(props: Iterable<KProperty1<T, *>>): Map<String, Any?> =
   props.filter { it.javaField != null }.associate { it.name to it.javaField?.apply { trySetAccessible() }?.get(this) }
 
-inline fun <reified T: Any> ResultSet.fromValues(vararg values: Pair<KProperty1<T, *>, Any?>) = fromValues(T::class, *values)
+inline fun <reified T: Any> ResultSet.fromValues(vararg provided: Pair<KProperty1<T, *>, Any?>) = fromValues(T::class, *provided)
 
-fun <T: Any> ResultSet.fromValues(type: KClass<T>, vararg values: Pair<KProperty1<T, *>, Any?>) = type.primaryConstructor!!.let { constructor ->
-  val extraArgs = values.associate { it.first.name to it.second }
+fun <T: Any> ResultSet.fromValues(type: KClass<T>, vararg provided: Pair<KProperty1<T, *>, Any?>) = type.primaryConstructor!!.let { constructor ->
+  val extraArgs = provided.associate { it.first.name to it.second }
   val args = constructor.parameters.associateWith {
     if (extraArgs.containsKey(it.name)) extraArgs[it.name] else JdbcConverter.from(getObject(it.name), it.type)
   }
