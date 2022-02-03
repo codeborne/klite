880178455433a04313111505a41f22e55864bfbd
Anton Keks <anton@codeborne.com>
2022-02-03 14:51:29 +0200

    update liquibase and slf4j versions

--- liquibase/build.gradle.kts
+++ liquibase/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
   api(project(":server"))
-  api("org.slf4j:jul-to-slf4j:1.7.30")
-  api("org.liquibase:liquibase-core:4.6.2")
+  api("org.slf4j:jul-to-slf4j:1.7.33")
+  api("org.liquibase:liquibase-core:4.7.1")
 }
