222f5eac8fdab2ec2309cdb6d317266d5b311352
Anton Keks <anton@codeborne.com>
2022-02-03 14:58:09 +0200

    update also Hikari and get rid of older slf4j versions

--- jdbc/build.gradle.kts
+++ jdbc/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
   api(project(":server"))
-  api("com.zaxxer:HikariCP:5.0.0")
   api(project(":slf4j"))
+  api("com.zaxxer:HikariCP:5.0.1") {
+    exclude("org.slf4j")
+  }
 }
--- liquibase/build.gradle.kts
+++ liquibase/build.gradle.kts
@@ -1,5 +1,8 @@
 dependencies {
   api(project(":server"))
+  api(project(":slf4j"))
   api("org.slf4j:jul-to-slf4j:1.7.33")
-  api("org.liquibase:liquibase-core:4.7.1")
+  api("org.liquibase:liquibase-core:4.7.1") {
+    exclude("org.slf4j")
+  }
 }
