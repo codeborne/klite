388aa399a634651ca37e027bcd081d0ce660fee7
Anton Keks <anton@codeborne.com>
2022-04-04 21:39:27 +0300

    add operator get/set functions to Config

--- server/src/klite/Config.kt
+++ server/src/klite/Config.kt
@@ -11,13 +11,16 @@ object Config {
   fun isActive(conf: String) = active.contains(conf)
   fun isAnyActive(vararg confs: String) = active.any { confs.contains(it) }
 
+  operator fun get(env: String) = required(env)
+  operator fun set(env: String, value: String) = System.setProperty(env, value)
+
   fun useEnvFile(file: File = File(".env"), force: Boolean = false) {
     if (!force && !file.exists()) return logger().info("No ${file.absolutePath} found, skipping")
     file.forEachLine {
       val line = it.trim()
       if (line.isNotEmpty() && !line.startsWith('#'))
         line.split('=', limit = 2).let { (key, value) ->
-          if (force || optional(key) == null) System.setProperty(key, value)
+          if (force || optional(key) == null) set(key, value)
         }
     }
   }
