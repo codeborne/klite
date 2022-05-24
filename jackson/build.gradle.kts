3aff193dded76826703a757340305e128d339cc2
Anton Keks <anton@codeborne.com>
2022-05-24 14:01:20 +0300

    update jackson

--- jackson/build.gradle.kts
+++ jackson/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
   api(project(":server"))
-  api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.2")
-  api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2") {
+  api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
+  api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3") {
     exclude("org.jetbrains.kotlin")
   }
 }
