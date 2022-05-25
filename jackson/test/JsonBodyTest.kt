add4dfea944f61ed3e90ae9a6e6d0ec8e47d9d28
Anton Keks <anton@codeborne.com>
2022-05-25 12:22:40 +0300

    add test with value class example (Jackson doesn't work with @JvmInline yet...)

--- jackson/test/JsonBodyTest.kt
+++ jackson/test/JsonBodyTest.kt
@@ -2,6 +2,9 @@ package klite.json
 
 import ch.tutteli.atrium.api.fluent.en_GB.toEqual
 import ch.tutteli.atrium.api.verbs.expect
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonCreator.Mode.DELEGATING
+import com.fasterxml.jackson.annotation.JsonValue
 import com.fasterxml.jackson.databind.exc.ValueInstantiationException
 import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
 import klite.ErrorResponse
@@ -13,12 +16,12 @@ class JsonBodyTest {
   val jsonBody = JsonBody()
 
   @Test fun `can create Kotlin classes`() {
-    val someData = jsonBody.parse("""{"hello":"World"}""".byteInputStream(), SomeData::class)
-    expect(someData).toEqual(SomeData("World"))
+    val someData = jsonBody.parse("""{"hello":"World", "nullableValue": "Value"}""".byteInputStream(), SomeData::class)
+    expect(someData).toEqual(SomeData("World", nullableValue = Value("Value")))
   }
 
-  @Test fun `parses empty strings to null`() {
-    val someData = jsonBody.parse("""{"hello":"World", "nullable": ""}""".byteInputStream(), SomeData::class)
+  @Test fun `coerces empty strings as nulls`() {
+    val someData = jsonBody.parse("""{"hello":"World", "nullable": "", "nullableValue": ""}""".byteInputStream(), SomeData::class)
     expect(someData).toEqual(SomeData("World"))
   }
 
@@ -41,9 +44,12 @@ class JsonBodyTest {
   }
 }
 
-data class SomeData(val hello: String, val nullable: String? = null) {
+data class SomeData(val hello: String, val nullable: String? = null, val nullableValue: Value? = null) {
   init {
     require(!hello.contains("Illegal")) { "Illegal stuff in hello" }
   }
 }
 
+// TODO: register values classes with klite Converter if they have @JsonCreator and @JsonValue annotations
+// then they will also be supported by JdbcConverter
+data class Value @JsonCreator(mode = DELEGATING) constructor(@JsonValue val value: String)
