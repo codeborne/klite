c7f11ff57840ff2bb00f356f1344c67dac8be5df
Anton Keks <anton@codeborne.com>
2022-05-30 10:33:07 +0300

    detect browser name & version and log it with requests

--- server/src/klite/Utils.kt
+++ server/src/klite/Utils.kt
@@ -30,3 +30,16 @@ val URI.queryParams: Params get() = urlDecodeParams(rawQuery)
 operator fun URI.plus(suffix: String) = URI(toString() + suffix)
 
 fun String?.trimToNull() = this?.trim()?.takeIf { it.isNotEmpty() }
+
+val HttpExchange.browser: String? get() = detectBrowser(header("User-Agent"))
+
+fun detectBrowser(userAgent: String?) = userAgent?.run {
+  fun String.detect(browser: String): String? {
+    val p = indexOf(browser)
+    return if (p >= 0) substring(p, indexOf(' ', startIndex = p + 1).takeIf { it > 0 } ?: length) else null
+  }
+
+  (if (contains("Mobile")) "Mobile/" else "") + (
+    detect("Edg") ?: detect("Chrome") ?: detect("Firefox") ?: detect("Trident") ?: detect("MSIE") ?:
+    if (contains("Safari")) detect("Version")?.replace("Version", "Safari") else split(' ', ';').find { it.contains("bot") } ?: this)
+}
---
+++ server/test/klite/BrowserTest.kt
@@ -0,0 +1,17 @@
+package klite
+
+import ch.tutteli.atrium.api.fluent.en_GB.toEqual
+import ch.tutteli.atrium.api.verbs.expect
+import org.junit.jupiter.api.Test
+
+class BrowserTest {
+  @Test fun `detects browser name and version`() {
+    expect(detectBrowser("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36")).toEqual("Chrome/100.0.4896.88")
+    expect(detectBrowser("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15")).toEqual("Safari/15.4")
+    expect(detectBrowser("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50")).toEqual("Edg/100.0.1185.50")
+    expect(detectBrowser("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0")).toEqual("Firefox/99.0")
+    expect(detectBrowser("Mozilla/5.0+(compatible; UptimeRobot/2.0; http://www.uptimerobot.com/)")).toEqual("UptimeRobot/2.0")
+    expect(detectBrowser("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")).toEqual("Googlebot/2.1")
+    expect(detectBrowser("Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")).toEqual("Mobile/Safari/13.0.3")
+  }
+}
