7f1beb6b465b0ef3066c121960561951893dd23f
Anton Keks, Mihhail Kornõšev <anton@codeborne.com, mihhail@codeborne.com>
2022-05-31 17:43:12 +0300

    make decorator functions usable outside of klite if needed

--- server/src/klite/Decorator.kt
+++ server/src/klite/Decorator.kt
@@ -3,14 +3,14 @@ package klite
 typealias Handler = suspend HttpExchange.() -> Any?
 typealias Decorator = suspend (exchange: HttpExchange, handler: Handler) -> Any?
 
-internal fun Decorator.wrap(handler: Handler): Handler = { invoke(this, handler) }
-internal fun List<Decorator>.wrap(handler: Handler) = foldRight(handler) { d, h -> d.wrap(h) }
+fun Decorator.wrap(handler: Handler): Handler = { invoke(this, handler) }
+fun List<Decorator>.wrap(handler: Handler) = foldRight(handler) { d, h -> d.wrap(h) }
 
 fun interface Before {
   suspend fun before(exchange: HttpExchange)
 }
 
-internal fun Before.toDecorator(): Decorator = { ex, next ->
+fun Before.toDecorator(): Decorator = { ex, next ->
   before(ex); next(ex)
 }
 
@@ -18,10 +18,10 @@ fun interface After {
   suspend fun after(exchange: HttpExchange, exception: Exception?)
 }
 
-internal fun After.toDecorator(): Decorator = { ex, next ->
+fun After.toDecorator(): Decorator = { ex, next ->
   try { next(ex); after(ex, null) }
   catch (e: Exception) { after(ex, e) }
 }
 
-internal fun Registry.requireAllDecorators() =
+fun Registry.requireAllDecorators() =
   requireAll<Before>().map { it.toDecorator() } + requireAll() + requireAll<After>().map { it.toDecorator() }
