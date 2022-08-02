31ccb001718b16d78f905f261bb94cefff6f5fc5
Markus Tammeoja, Anton Keks <markus@codeborne.com, anton@codeborne.com>
2022-08-02 15:41:46 +0300

    upgrade Kotlin to 1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.7.0"
+  kotlin("jvm") version "1.7.10"
 }
 
 allprojects {
   group = rootProject.name
-  version = "0.9-SNAPSHOT"
+  version = "1.0-SNAPSHOT"
 }
 
 subprojects {
