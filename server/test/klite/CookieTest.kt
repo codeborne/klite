9f2f8af6d18e90703e43a145e91080012fe2b188
Anton Keks, Markus Tammeoja <anton@codeborne.com, markus@codeborne.com>
2022-08-03 11:53:46 +0300

    set cookies with Path=/ by default - cookies are rarely useful without the Path attribute

--- server/test/klite/CookieTest.kt
+++ server/test/klite/CookieTest.kt
@@ -8,22 +8,23 @@ import java.time.Instant.ofEpochSecond
 
 class CookieTest {
   @Test fun plain() {
-    expect(Cookie("Hello", "Wörld").toString()).toEqual("Hello=W%C3%B6rld")
-  }
-
-  @Test fun expires() {
-    expect(Cookie("Hello", "World", expires = ofEpochSecond(1634800860)).toString()).toEqual("Hello=World; Expires=Thu, 21 Oct 2021 07:21:00 GMT")
-  }
-
-  @Test fun domain() {
-    expect(Cookie("Hello", "World", domain = "github.com").toString()).toEqual("Hello=World; Domain=github.com")
+    expect(Cookie("Hello", "Wörld", path = null).toString()).toEqual("Hello=W%C3%B6rld")
   }
 
   @Test fun path() {
+    expect(Cookie("Hello", "World").toString()).toEqual("Hello=World; Path=/")
     expect(Cookie("Hello", "World", path = "/hello").toString()).toEqual("Hello=World; Path=/hello")
   }
 
+  @Test fun expires() {
+    expect(Cookie("Hello", "World", path = null, expires = ofEpochSecond(1634800860)).toString()).toEqual("Hello=World; Expires=Thu, 21 Oct 2021 07:21:00 GMT")
+  }
+
+  @Test fun domain() {
+    expect(Cookie("Hello", "World", domain = "github.com").toString()).toEqual("Hello=World; Path=/; Domain=github.com")
+  }
+
   @Test fun attrs() {
-    expect(Cookie("Hello", "World", httpOnly = true, secure = true, sameSite = None).toString()).toEqual("Hello=World; HttpOnly; Secure; SameSite=None")
+    expect(Cookie("Hello", "World", path = null, httpOnly = true, secure = true, sameSite = None).toString()).toEqual("Hello=World; HttpOnly; Secure; SameSite=None")
   }
 }
