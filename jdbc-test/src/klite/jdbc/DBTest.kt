26aaabf16eda9088f1bc24f346a21a6156ee5dd1
Anton Keks, Markus Tammeoja <anton@codeborne.com, markus@codeborne.com>
2022-08-24 15:10:56 +0300

    remove DBTest LocalDate constants
    In order to discourage using them from unexpected places (e.g. E2E tests), because loading of DBTest reinitializes the ENV

--- jdbc-test/src/klite/jdbc/DBTest.kt
+++ jdbc-test/src/klite/jdbc/DBTest.kt
@@ -4,15 +4,10 @@ import com.zaxxer.hikari.pool.HikariPool
 import klite.Config
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
-import java.time.LocalDate
 import javax.sql.DataSource
 
 abstract class DBTest {
   companion object {
-    val today = LocalDate.now()
-    val tomorrow = today.plusDays(1)
-    val yesterday = today.minusDays(1)
-
     init {
       Config.useEnvFile()
       Config["ENV"] = "test"
