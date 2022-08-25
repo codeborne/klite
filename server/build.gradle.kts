51aefdb9c97a60911e3767000eb98cd4a6df10a5
Anton Keks, Markus Tammeoja <anton@codeborne.com, markus@codeborne.com>
2022-08-25 10:38:04 +0300

    update coroutines library

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,4 +1,4 @@
 dependencies {
   api("org.jetbrains.kotlin:kotlin-reflect")
-  api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.0")
+  api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
 }
