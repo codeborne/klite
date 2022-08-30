7021e9436e0970a7ce01eea411f20e3bc4cd2a9e
Anton Keks <anton@codeborne.com>
2022-08-30 13:59:04 +0300

    update liquibase

--- liquibase/build.gradle.kts
+++ liquibase/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
   implementation(project(":server"))
   api(project(":slf4j"))
   api("org.slf4j:jul-to-slf4j:1.7.36")
-  api("org.liquibase:liquibase-core:4.9.1") {
+  api("org.liquibase:liquibase-core:4.15.0") {
     exclude("org.slf4j")
   }
 }
