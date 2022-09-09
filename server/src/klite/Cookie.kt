0f93cbff6d4df4fccdc19414a22e655d8dff69ba
Anton Keks <anton@codeborne.com>
2022-09-09 15:04:17 +0300

    add Cookie Max-Age parameter support (takes precedence over Expires according to RFC)

--- server/src/klite/Cookie.kt
+++ server/src/klite/Cookie.kt
@@ -4,16 +4,16 @@ import java.time.Instant
 import java.time.ZoneOffset.UTC
 import java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME
 
-data class Cookie(val name: String, val value: String, val expires: Instant? = null,
+data class Cookie(val name: String, val value: String, val expires: Instant? = null, val maxAgeSec: Int? = null,
                   val path: String? = "/", val domain: String? = null,
                   val httpOnly: Boolean = false, val secure: Boolean = false, val sameSite: SameSite? = null) {
-
   enum class SameSite { Lax, Strict, None }
 
   override fun toString() = "$name=${value.urlEncode()}" + (sequenceOf(
-    expires?.let { "Expires=" + RFC_1123_DATE_TIME.format(expires.atOffset(UTC)) },
-    path?.let { "Path=$path" },
-    domain?.let { "Domain=$domain" },
+    expires?.let { "Expires=" + RFC_1123_DATE_TIME.format(it.atOffset(UTC)) },
+    maxAgeSec?.let { "Max-Age=$it" },
+    path?.let { "Path=$it" },
+    domain?.let { "Domain=$it" },
     "HttpOnly".takeIf { httpOnly }, "Secure".takeIf { secure }, sameSite?.let { "SameSite=$it" }
   ).filterNotNull().joinToString("") { "; $it" })
 }
