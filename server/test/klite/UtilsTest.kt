ccc4fb6dbcebbe04c17b0dc6d123cd53fd0e711e
Anton Keks <anton@codeborne.com>
2022-09-12 12:58:29 +0300

    support for URL hashes (fragments), when adding more params

--- server/test/klite/UtilsTest.kt
+++ server/test/klite/UtilsTest.kt
@@ -3,8 +3,13 @@ package klite
 import ch.tutteli.atrium.api.fluent.en_GB.toEqual
 import ch.tutteli.atrium.api.verbs.expect
 import org.junit.jupiter.api.Test
+import java.net.URI
 
 class UtilsTest {
+  @Test fun urlPlus() {
+    expect(URI("https://codeborne.com/#ref") + mapOf("hello" to "world")).toEqual(URI("https://codeborne.com/?hello=world#ref"))
+  }
+
   @Test fun urlParams() {
     val params = mapOf("Hello" to "Wörld", "1" to "2", "null" to null)
     expect(urlEncodeParams(params)).toEqual("Hello=W%C3%B6rld&1=2")
