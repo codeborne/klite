daf7610858fa131293f6bda8fe38dc3448dcdbb7
Anton Keks <anton@codeborne.com>
2022-09-13 15:47:33 +0300

    allow URI + operator with non-strings, the same way as urlEncodeParams()

--- server/src/klite/Utils.kt
+++ server/src/klite/Utils.kt
@@ -14,15 +14,14 @@ fun String.base64Encode() = toByteArray().base64Encode()
 fun String.base64Decode() = Base64.getDecoder().decode(this)
 
 typealias Params = Map<String, String?>
+val URI.queryParams: Params get() = urlDecodeParams(rawQuery)
 
 fun urlEncodeParams(params: Map<String, Any?>) = params.mapNotNull { e -> e.value?.let { e.key + "=" + it.toString().urlEncode() } }.joinToString("&")
 fun urlDecodeParams(params: String?): Params = params?.split('&')?.associate(::keyValue) ?: emptyMap()
 internal fun keyValue(s: String) = s.split('=', limit = 2).let { it[0] to it.getOrNull(1)?.urlDecode() }
 
-val URI.queryParams: Params get() = urlDecodeParams(rawQuery)
-
 operator fun URI.plus(suffix: String) = URI(toString().substringBefore("#") + suffix + (fragment?.let { "#$it" } ?: ""))
-operator fun URI.plus(params: Params) = plus((if (rawQuery == null) "?" else "&") + urlEncodeParams(params))
+operator fun URI.plus(params: Map<String, Any?>) = plus((if (rawQuery == null) "?" else "&") + urlEncodeParams(params))
 
 fun String?.trimToNull() = this?.trim()?.takeIf { it.isNotEmpty() }
 
