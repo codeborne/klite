357c66cb1376e967ec5461f5b082b5b38feee164
Anton Keks <anton@codeborne.com>
2022-09-20 00:06:29 +0300

    use existing sample server in integration test

--- sample/src/Launcher.kt
+++ sample/src/Launcher.kt
@@ -5,51 +5,60 @@ import klite.jdbc.RequestTransactionHandler
 import klite.jdbc.startDevDB
 import klite.json.JsonBody
 import kotlinx.coroutines.delay
+import java.net.InetSocketAddress
+import java.net.http.HttpClient
 import java.nio.file.Path
+import java.time.Duration.ofSeconds
 
 fun main() {
+  sampleServer().start()
+}
+
+fun sampleServer(port: Int = 8080) = Server(listen = InetSocketAddress(port)).apply {
   Config.useEnvFile()
+  use<JsonBody>() // enables parsing/sending of application/json requests/responses, depending on the Accept header
 
-  Server().apply {
-    use<JsonBody>() // enables parsing/sending of application/json requests/responses, depending on the Accept header
+  if (Config.isDev) startDevDB() // start docker-compose db automatically
+  use<DBModule>() // configure a DataSource
+  use<RequestTransactionHandler>() // runs each request in a transaction
 
-    if (Config.isDev) startDevDB() // start docker-compose db automatically
-    use<DBModule>() // configure a DataSource
-    use<RequestTransactionHandler>() // runs each request in a transaction
+  assets("/", AssetsHandler(Path.of("public"), useIndexForUnknownPaths = true))
 
-    assets("/", AssetsHandler(Path.of("public"), useIndexForUnknownPaths = true))
+  register(HttpClient.newBuilder().connectTimeout(ofSeconds(5)).build())
 
-    before(require<AdminChecker>())
+  before<AdminChecker>()
+  after { ex, err -> ex.header("X-Error", err?.message ?: "none") }
 
-    context("/hello") {
-      get { "Hello World" }
+  context("/hello") {
+    get { "Hello World" }
 
-      get("/delay") {
-        delay(1000)
-        "Waited for 1 sec"
-      }
-
-      get("/failure") { error("Failure") }
-
-      get("/admin") @AdminOnly {
-        "Only for admins" // TODO: Kotlin bug: suspend lambda annotations don't work yet
-      }
-
-      get("/:param") {
-        "Path: ${path("param")}, Query: $queryParams"
-      }
-
-      post("/post") {
-        data class JsonRequest(val required: String, val hello: String = "World")
-        body<JsonRequest>()
-      }
+    get("/delay") {
+      delay(1000)
+      "Waited for 1 sec"
     }
 
-    context("/api") {
-      useOnly<JsonBody>() // in case only json should be supported in this context
-      annotated<MyRoutes>() // read routes from an annotated class - such classes are easier to unit-test
+    get("/failure") { error("Failure") }
+
+    get("/admin") @AdminOnly {
+      "Only for admins" // TODO: Kotlin bug: suspend lambda annotations don't work yet
     }
 
-    start()
+    get("/param/:param") {
+      "Path: ${path("param")}, Query: $queryParams"
+    }
+
+    post("/post") {
+      data class JsonRequest(val required: String, val hello: String = "World")
+      body<JsonRequest>()
+    }
+
+    decorator { ex, h -> "<${h(ex)}>" }
+    get("/decorated") { "!!!" }
+  }
+
+  context("/api") {
+    useOnly<JsonBody>() // in case only json should be supported in this context
+    annotated<MyRoutes>() // read routes from an annotated class - such classes are easier to unit-test
   }
 }
+
--- sample/test/ServerIntegrationTest.kt
+++ sample/test/ServerIntegrationTest.kt
@@ -1,47 +1,28 @@
 package klite.sample
 
 import MyData
-import MyRoutes
 import ch.tutteli.atrium.api.fluent.en_GB.messageToContain
 import ch.tutteli.atrium.api.fluent.en_GB.toEqual
 import ch.tutteli.atrium.api.fluent.en_GB.toThrow
 import ch.tutteli.atrium.api.verbs.expect
-import klite.Config
-import klite.Server
-import klite.annotations.annotated
-import klite.jdbc.DBModule
-import klite.json.JsonBody
 import klite.json.JsonHttpClient
-import klite.register
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
+import sampleServer
 import java.io.IOException
-import java.net.InetSocketAddress
-import java.net.http.HttpClient
 import java.net.http.HttpRequest.BodyPublishers.noBody
-import java.time.Duration.ofSeconds
 
 class ServerIntegrationTest {
   @Test fun requests() {
     val port = (Math.random() * 60000 + 1000).toInt()
-    val server = Server(listen = InetSocketAddress(port)).apply {
-      Config.useEnvFile()
-      use(DBModule())
-      registry.register(HttpClient.newBuilder().connectTimeout(ofSeconds(5)).build())
-      use(JsonBody())
-      context("/") {
-        get("hello") { "Hello" }
-      }
-      context("/api") {
-        useOnly<JsonBody>()
-        annotated<MyRoutes>()
-      }
-      start(gracefulStopDelaySec = -1)
-    }
+    val server = sampleServer(port).apply { start(gracefulStopDelaySec = -1) }
 
     val http = JsonHttpClient("http://localhost:$port", registry = server.registry)
     runBlocking {
-      expect(http.get<String>("/hello")).toEqual("\"Hello\"")
+      expect(http.get<String>("/hello")).toEqual("\"Hello World\"")
+      expect(http.get<String>("/hello/param/123456?query=123")).toEqual("\"Path: 123456, Query: {query=123}\"")
+      expect(http.get<String>("/hello/decorated")).toEqual("\"<!!!>\"")
+
       expect(http.get<MyData>("/api/hello")).toEqual(MyData("Hello"))
       expect(http.request<Unit>("/api/hello") { method("HEAD", noBody()) }).toEqual(Unit)
       expect(http.get<Unit>("/api/hello/suspend204")).toEqual(Unit)
