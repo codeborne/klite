5546a1428f21eac80bc7da3feb8dee581ebf68f2
Anton Keks <anton@codeborne.com>
2022-09-26 21:06:34 +0300

    update junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,12 +18,12 @@ subprojects {
   }
 
   dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    testImplementation("ch.tutteli.atrium:atrium-fluent-en_GB:0.17.0") {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testImplementation("ch.tutteli.atrium:atrium-fluent-en_GB:0.18.0") {
       exclude("org.jetbrains.kotlin")
     }
-    testImplementation("io.mockk:mockk:1.12.1") {
+    testImplementation("io.mockk:mockk:1.13.0") {
       exclude("org.jetbrains.kotlin")
     }
   }
