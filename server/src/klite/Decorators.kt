f8972191bb68b54d3da3153a0bd2715a32bdaa4f
Anton Keks <anton@codeborne.com>
2022-10-14 11:54:09 +0300

    rename vars

--- server/src/klite/Decorators.kt
+++ server/src/klite/Decorators.kt
@@ -10,17 +10,17 @@ fun interface Before {
   suspend fun before(exchange: HttpExchange)
 }
 
-fun Before.toDecorator(): Decorator = { ex, next ->
-  before(ex); next(ex)
+fun Before.toDecorator(): Decorator = { ex, handler ->
+  before(ex); handler(ex)
 }
 
 fun interface After {
   suspend fun after(exchange: HttpExchange, error: Throwable?)
 }
 
-fun After.toDecorator(): Decorator = { ex, next ->
+fun After.toDecorator(): Decorator = { ex, handler ->
   var error: Throwable? = null
-  try { next(ex) }
+  try { handler(ex) }
   catch (e: Throwable) { error = e; throw e }
   finally { after(ex, error) }
 }
