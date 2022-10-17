80c95c1fbe4d550a3eca34c3a2c94d06166ae546
Anton Keks <anton@codeborne.com>
2022-10-17 13:11:07 +0300

    add an optional where parameter to count()

--- jdbc/src/BaseRepository.kt
+++ jdbc/src/BaseRepository.kt
@@ -6,5 +6,5 @@ abstract class BaseRepository(protected val db: DataSource, val table: String) {
   protected val orderAsc = "order by createdAt"
   protected val orderDesc = "$orderAsc desc"
 
-  fun count(): Int = db.select("select count(*) from $table", emptyMap()) { getInt(1) }.first()
+  fun count(where: Map<String, Any?> = emptyMap()): Int = db.select("select count(*) from $table", where) { getInt(1) }.first()
 }
