1dbc354a9e2e2afc02f9283decc87d825a2aeb08
Tõnis Aruste <tonis@codeborne.com>
2022-10-26 15:20:31 +0300

    Use exception's message as log message so that Google Cloud wouldn't show empty log rows

--- server/src/klite/Logger.kt
+++ server/src/klite/Logger.kt
@@ -7,4 +7,4 @@ fun Any.logger(name: String = javaClass.name): System.Logger = System.getLogger(
 inline fun System.Logger.info(msg: String) = log(INFO, msg)
 inline fun System.Logger.warn(msg: String) = log(WARNING, msg)
 inline fun System.Logger.error(msg: String, e: Throwable? = null) = log(ERROR, msg, e)
-inline fun System.Logger.error(e: Throwable) = log(ERROR, "", e)
+inline fun System.Logger.error(e: Throwable) = log(ERROR, e.message ?: e::class.qualifiedName ?: "", e)
