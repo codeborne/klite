1e8d76500547051573c5b1244101481afd425dd9
Anton Keks <anton@codeborne.com>
2022-12-02 15:57:22 +0200

    make repository methods open, so they can be overridden

--- jdbc/src/BaseRepository.kt
+++ jdbc/src/BaseRepository.kt
@@ -9,7 +9,7 @@ abstract class BaseRepository(protected val db: DataSource, val table: String) {
   protected val orderAsc = "order by createdAt"
   protected val orderDesc = "$orderAsc desc"
 
-  fun count(where: Map<String, Any?> = emptyMap()): Int = db.select("select count(*) from $table", where) { getInt(1) }.first()
+  open fun count(where: Map<String, Any?> = emptyMap()): Int = db.select("select count(*) from $table", where) { getInt(1) }.first()
 }
 
 abstract class CRUDRepository<E: Entity>(db: DataSource, table: String): BaseRepository(db, table) {
@@ -19,8 +19,8 @@ abstract class CRUDRepository<E: Entity>(db: DataSource, table: String): BaseRep
   protected open fun ResultSet.createEntity(): E = fromValues(entityClass)
   protected open fun E.writeEntity() = toValues()
 
-  fun get(id: UUID): E = db.query(table, id) { createEntity() }
-  fun save(entity: E) = db.upsert(table, entity.writeEntity())
-  fun list(where: Map<String, Any?> = emptyMap(), order: String = orderDesc): List<E> =
+  open fun get(id: UUID): E = db.query(table, id) { createEntity() }
+  open fun save(entity: E) = db.upsert(table, entity.writeEntity())
+  open fun list(where: Map<String, Any?> = emptyMap(), order: String = orderDesc): List<E> =
     db.query(table, where, order) { createEntity() }
 }
