c3b75f9deff315fe6642f73450ba905e0174db5c
Anton Keks <anton@codeborne.com>
2022-12-02 15:51:40 +0200

    introduce CRUDRepository base class

--- jdbc/src/BaseRepository.kt
+++ jdbc/src/BaseRepository.kt
@@ -1,10 +1,26 @@
 package klite.jdbc
 
+import java.sql.ResultSet
+import java.util.*
 import javax.sql.DataSource
+import kotlin.reflect.KClass
 
 abstract class BaseRepository(protected val db: DataSource, val table: String) {
   protected val orderAsc = "order by createdAt"
   protected val orderDesc = "$orderAsc desc"
 
   fun count(where: Map<String, Any?> = emptyMap()): Int = db.select("select count(*) from $table", where) { getInt(1) }.first()
+}
+
+abstract class CRUDRepository<E: Entity>(db: DataSource, table: String): BaseRepository(db, table) {
+  @Suppress("UNCHECKED_CAST")
+  private val entityClass = this::class.supertypes.first().arguments.first().type!!.classifier as KClass<E>
+
+  protected open fun ResultSet.createEntity(): E = fromValues(entityClass)
+  protected open fun E.writeEntity() = toValues()
+
+  fun get(id: UUID): E = db.query(table, id) { createEntity() }
+  fun save(entity: E) = db.upsert(table, entity.writeEntity())
+  fun list(where: Map<String, Any?> = emptyMap(), order: String = orderDesc): List<E> =
+    db.query(table, where, order) { createEntity() }
 }
--- sample/src/users/UserRepository.kt
+++ sample/src/users/UserRepository.kt
@@ -1,11 +1,9 @@
 package users
 
-import klite.jdbc.*
-import java.util.*
+import klite.jdbc.CRUDRepository
+import klite.jdbc.query
 import javax.sql.DataSource
 
-class UserRepository(db: DataSource) : BaseRepository(db, "users") {
-  fun save(user: User) = db.insert(table, user.toValues())
-  fun get(id: UUID): User = db.query(table, id) { fromValues() }
-  fun by(email: Email): User? = db.query(table, mapOf("email" to email)) { fromValues<User>() }.firstOrNull()
+class UserRepository(db: DataSource): CRUDRepository<User>(db, "users") {
+  fun by(email: Email): User? = db.query(table, mapOf("email" to email)) { createEntity() }.firstOrNull()
 }
