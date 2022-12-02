0f42c881d04e3ebfd7366fae4ed89d877577746c
Anton Keks <anton@codeborne.com>
2022-12-02 16:01:01 +0200

    rename mapper and persister, to be more similar with the previous code

--- jdbc/src/BaseRepository.kt
+++ jdbc/src/BaseRepository.kt
@@ -16,11 +16,11 @@ abstract class CRUDRepository<E: Entity>(db: DataSource, table: String): BaseRep
   @Suppress("UNCHECKED_CAST")
   private val entityClass = this::class.supertypes.first().arguments.first().type!!.classifier as KClass<E>
 
-  protected open fun ResultSet.createEntity(): E = fromValues(entityClass)
-  protected open fun E.writeEntity() = toValues()
+  protected open fun ResultSet.mapper(): E = fromValues(entityClass)
+  protected open fun E.persister() = toValues()
 
-  open fun get(id: UUID): E = db.query(table, id) { createEntity() }
-  open fun save(entity: E) = db.upsert(table, entity.writeEntity())
+  open fun get(id: UUID): E = db.query(table, id) { mapper() }
+  open fun save(entity: E) = db.upsert(table, entity.persister())
   open fun list(where: Map<String, Any?> = emptyMap(), order: String = orderDesc): List<E> =
-    db.query(table, where, order) { createEntity() }
+    db.query(table, where, order) { mapper() }
 }
--- sample/src/users/UserRepository.kt
+++ sample/src/users/UserRepository.kt
@@ -5,5 +5,5 @@ import klite.jdbc.query
 import javax.sql.DataSource
 
 class UserRepository(db: DataSource): CRUDRepository<User>(db, "users") {
-  fun by(email: Email): User? = db.query(table, mapOf("email" to email)) { createEntity() }.firstOrNull()
+  fun by(email: Email): User? = db.query(table, mapOf("email" to email)) { mapper() }.firstOrNull()
 }
