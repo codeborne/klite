21caad148d4cc615a48ed30f68e9fd18f2757505
Anton Keks <anton@codeborne.com>
2022-12-15 11:51:41 +0200

    add a note that exclusions are not propagated

--- liquibase/build.gradle.kts
+++ liquibase/build.gradle.kts
@@ -3,7 +3,7 @@ dependencies {
   api(project(":slf4j"))
   api("org.slf4j:jul-to-slf4j:2.0.5")
   api("org.liquibase:liquibase-core:4.17.2") {
-    // include required sub-dependencies in your project if needed
+    // TODO: unfortunately these are not propagated to projects depending on klite-liquibase...
     exclude("org.slf4j")
     exclude("com.opencsv")
     exclude("javax.xml.bind")
