c974b4e00de939fb183fa5126d3326f099257480
Anton Keks <anton@codeborne.com>
2022-12-15 16:45:50 +0200

    update liquibase

--- liquibase/build.gradle.kts
+++ liquibase/build.gradle.kts
@@ -1,12 +1,11 @@
 dependencies {
   implementation(project(":server"))
-  api(project(":slf4j"))
-  api("org.slf4j:jul-to-slf4j:2.0.5")
-  api("org.liquibase:liquibase-core:4.17.2") {
+  implementation(project(":slf4j"))
+  implementation("org.slf4j:jul-to-slf4j:2.0.5")
+  api("org.liquibase:liquibase-core:4.18.0") {
     // TODO: unfortunately these are not propagated to projects depending on klite-liquibase...
-    exclude("org.slf4j")
-    exclude("com.opencsv")
     exclude("javax.xml.bind")
     exclude("org.yaml")
+    exclude("com.opencsv")
   }
 }
