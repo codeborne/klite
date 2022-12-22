0ac719f4f7abb01d3f45b599b366c117a0ff549a
Anton Keks <anton@codeborne.com>
2022-12-22 22:52:17 +0200

    introduce onChange enum

--- jdbc/src/DBMigrator.kt
+++ jdbc/src/DBMigrator.kt
@@ -1,7 +1,9 @@
 package klite.jdbc
 
 import klite.info
+import klite.jdbc.ChangeSet.OnChange.*
 import klite.logger
+import klite.warn
 import java.io.Reader
 import java.lang.Thread.currentThread
 import java.sql.SQLException
@@ -61,14 +63,24 @@ open class DBMigrator(
         throw MigrationException("Cannot execute dangling SQL without a changeset:\n" + changeSet.sql)
       return
     }
+    var run = true
     executed[changeSet.id]?.let {
-      if (it.checksum != changeSet.checksum)
-        throw MigrationException(changeSet, "has changed, add runOnChange:true or skipOnChange:true TODO")
-      return
+      if (it.checksum == changeSet.checksum) return
+      when (changeSet.onChange) {
+        FAIL -> throw MigrationException(changeSet, "has changed, use onChange: options")
+        SKIP -> return log.warn("Skipping changed $changeSet")
+        MARK_RAN -> {
+          log.warn("Marking changed $changeSet as ran")
+          run = false
+        }
+        RUN -> {}
+      }
     }
     try {
-      log.info("Executing $changeSet")
-      changeSet.statements.forEach { db.exec(it) }
+      if (run) {
+        log.info("Executing $changeSet")
+        changeSet.statements.forEach { db.exec(it) }
+      }
       repository.save(changeSet)
       tx.commit()
       log.info("Executed $changeSet")
@@ -83,8 +95,10 @@ data class ChangeSet(
   override val id: String,
   val filePath: String,
   val context: String? = null,
+  var checksum: Long = 0
 ): BaseEntity<String> {
   val separator: String? = ";"
+  val onChange: OnChange = FAIL
   val sql = StringBuilder()
 
   operator fun plusAssign(line: String) {
@@ -93,9 +107,9 @@ data class ChangeSet(
   }
 
   val statements get() = (separator?.let { sql.split(it).asSequence() } ?: sequenceOf(sql.toString()))
-    .map { it.trimEnd() }.filter { it.isNotEmpty() }
+    .map { it.trimEnd() }.filter { it.isNotEmpty() }.also { checksum = it.fold(0) { r, s -> r + s.hashCode() * 89 } }
 
-  val checksum: Long get() = statements.fold(0) { r, s -> r + s.hashCode() * 89 }
+  enum class OnChange { FAIL, RUN, SKIP, MARK_RAN }
 }
 
 class ChangeSetRepository(db: DataSource): BaseCrudRepository<ChangeSet, String>(db, "db_changelog") {
