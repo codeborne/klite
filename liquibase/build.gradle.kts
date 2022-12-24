4fbd31e27d1388c169ba607f52d98f10d24fc9d9
Anton Keks <anton@codeborne.com>
2022-12-24 13:33:29 +0200

    update comment

--- liquibase/build.gradle.kts
+++ liquibase/build.gradle.kts
@@ -3,7 +3,7 @@ dependencies {
   implementation(project(":slf4j"))
   implementation("org.slf4j:jul-to-slf4j:2.0.5")
   api("org.liquibase:liquibase-core:4.18.0") {
-    // TODO: unfortunately these are not propagated to projects depending on klite-liquibase...
+    // TODO: unfortunately excludes are not currently published by Jitpack: https://github.com/jitpack/jitpack.io/issues/5349
     exclude("javax.xml.bind")
     exclude("org.yaml")
     exclude("com.opencsv")
