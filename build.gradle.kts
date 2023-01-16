636413db769fd10d7ec7f91ae1727ec9c8efe592
Anton Keks <anton@codeborne.com>
2023-01-16 11:33:25 +0200

    cannot use pre-release language level for libraries, go back to 1.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,6 @@ subprojects {
   tasks.withType<KotlinCompile> {
     kotlinOptions {
       jvmTarget = "11"
-      languageVersion = "1.8"
       freeCompilerArgs += "-opt-in=kotlin.ExperimentalStdlibApi"
     }
   }
