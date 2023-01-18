ce0c7cc5895372f2580afc254759aefb4e97fc46
Anton Keks <anton@codeborne.com>
2023-01-18 23:38:41 +0200

    add onFail:SKIP by default for liquibase, because on subsequent runs liquibase tables may not exist anymore

--- jdbc/src/migrator/liquibase.sql
+++ jdbc/src/migrator/liquibase.sql
@@ -1,4 +1,4 @@
---changeset migrate-from-liquibase
+--changeset migrate-from-liquibase onFail:SKIP
 --a sample migration from Liquibase, will work if author:id pairs were unique across different files
 insert into db_changelog (id, filePath, context, createdAt)
   select author || ':' || id, filename, contexts, dateexecuted from databasechangelog;
