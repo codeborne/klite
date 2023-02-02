a3e992ea0d8c139bffa8ef99ee1625ce1dbae8ee
Anton Keks <anton@codeborne.com>
2023-02-02 18:51:35 +0200

    make timeout redefineable

--- jdbc/src/DockerCompose.kt
+++ jdbc/src/DockerCompose.kt
@@ -4,12 +4,14 @@ import klite.Config
 import java.lang.ProcessBuilder.Redirect.INHERIT
 import java.lang.Thread.sleep
 import kotlin.system.measureTimeMillis
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
-fun dockerCompose(vararg args: String) = ProcessBuilder(Config.optional("DOCKER_COMPOSE", "docker-compose"), *args)
+fun dockerCompose(vararg args: String) = ProcessBuilder(Config.optional("DOCKER_COMPOSE", "docker-compose2"), *args)
   .redirectErrorStream(true).redirectOutput(INHERIT).start().waitFor()
 
-fun startDevDB(service: String = Config.optional("DB_START", "db")) {
+fun startDevDB(service: String = Config.optional("DB_START", "db"), timeout: Duration = 2.seconds) {
   if (service.isEmpty()) return
   val ms = measureTimeMillis { dockerCompose("up", "-d", service) }
-  if (ms > 400) sleep(2000) // give the db more time to start listening
+  if (ms > timeout.inWholeMilliseconds / 5) sleep(timeout.inWholeMilliseconds) // give the db more time to start listening
 }
