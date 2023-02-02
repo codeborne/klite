923a0e92db4963b9595b55126bd6f8b81d238968
Anton Keks <anton@codeborne.com>
2023-02-02 18:57:35 +0200

    do not crash if startDevDB() fails, just write a warning

--- jdbc/src/DockerCompose.kt
+++ jdbc/src/DockerCompose.kt
@@ -1,17 +1,23 @@
 package klite.jdbc
 
 import klite.Config
+import klite.warn
+import java.io.IOException
 import java.lang.ProcessBuilder.Redirect.INHERIT
 import java.lang.Thread.sleep
 import kotlin.system.measureTimeMillis
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
-fun dockerCompose(vararg args: String) = ProcessBuilder(Config.optional("DOCKER_COMPOSE", "docker-compose2"), *args)
+fun dockerCompose(vararg args: String) = ProcessBuilder(Config.optional("DOCKER_COMPOSE", "docker-compose"), *args)
   .redirectErrorStream(true).redirectOutput(INHERIT).start().waitFor()
 
 fun startDevDB(service: String = Config.optional("DB_START", "db"), timeout: Duration = 2.seconds) {
   if (service.isEmpty()) return
-  val ms = measureTimeMillis { dockerCompose("up", "-d", service) }
-  if (ms > timeout.inWholeMilliseconds / 5) sleep(timeout.inWholeMilliseconds) // give the db more time to start listening
+  try {
+    val ms = measureTimeMillis { dockerCompose("up", "-d", service) }
+    if (ms > timeout.inWholeMilliseconds / 5) sleep(timeout.inWholeMilliseconds) // give the db more time to start listening
+  } catch (e: IOException) {
+    System.getLogger("startDevDB").warn("Failed to automatically start $service: $e")
+  }
 }
