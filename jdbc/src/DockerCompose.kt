0f6dae8f23936c8fa166e7dcd97878f580db31f3
Anton Keks <anton@codeborne.com>
2023-02-02 19:02:11 +0200

    make it possible to replace "docker-compose" with "docker compose"

--- jdbc/src/DockerCompose.kt
+++ jdbc/src/DockerCompose.kt
@@ -9,13 +9,13 @@ import kotlin.system.measureTimeMillis
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
-fun dockerCompose(vararg args: String) = ProcessBuilder(Config.optional("DOCKER_COMPOSE", "docker-compose"), *args)
+fun dockerCompose(command: String) = ProcessBuilder((Config.optional("DOCKER_COMPOSE", "docker-compose") + " " + command).split(' '))
   .redirectErrorStream(true).redirectOutput(INHERIT).start().waitFor()
 
 fun startDevDB(service: String = Config.optional("DB_START", "db"), timeout: Duration = 2.seconds) {
   if (service.isEmpty()) return
   try {
-    val ms = measureTimeMillis { dockerCompose("up", "-d", service) }
+    val ms = measureTimeMillis { dockerCompose("up -d $service") }
     if (ms > timeout.inWholeMilliseconds / 5) sleep(timeout.inWholeMilliseconds) // give the db more time to start listening
   } catch (e: IOException) {
     System.getLogger("startDevDB").warn("Failed to automatically start $service: $e")
