07854e0afafb67120a6f5affbd66e72306868c41
Anton Keks <anton@codeborne.com>
2023-02-20 22:52:25 +0200

    extract TypedHttpClient as a base class of different JsonHttpClients

--- jackson/src/JsonHttpClient.kt
+++ jackson/src/JsonHttpClient.kt
@@ -2,19 +2,14 @@ package klite.json
 
 import com.fasterxml.jackson.databind.json.JsonMapper
 import klite.*
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.future.await
+import klite.http.TypedHttpClient
 import java.io.IOException
-import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
-import java.net.http.HttpRequest.BodyPublishers.ofString
 import java.net.http.HttpResponse
-import java.net.http.HttpResponse.BodyHandlers.ofString
 import kotlin.reflect.KClass
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
-import kotlin.time.toJavaDuration
 
 typealias RequestModifier =  HttpRequest.Builder.() -> HttpRequest.Builder
 
@@ -24,81 +19,22 @@ typealias RequestModifier =  HttpRequest.Builder.() -> HttpRequest.Builder
  * or provide both http and json parameters to the constructor instead
  */
 class JsonHttpClient(
-  private val urlPrefix: String = "",
-  val reqModifier: RequestModifier = { this },
-  val errorHandler: (HttpResponse<*>, String) -> Nothing = { res, body -> throw IOException("Failed with ${res.statusCode()}: $body") },
-  val retryCount: Int = 0,
-  val retryAfter: Duration = 1.seconds,
-  private val maxLoggedLen: Int = 1000,
+  urlPrefix: String = "",
+  reqModifier: RequestModifier = { this },
+  errorHandler: (HttpResponse<*>, String) -> Nothing = { res, body -> throw IOException("Failed with ${res.statusCode()}: $body") },
+  retryCount: Int = 0,
+  retryAfter: Duration = 1.seconds,
+  maxLoggedLen: Int = 1000,
   registry: Registry? = null,
-  val http: HttpClient = registry!!.require(),
+  http: HttpClient = registry!!.require(),
   val json: JsonMapper = registry!!.require()
-) {
-  val logger = logger(Exception().stackTrace.first { it.className !== javaClass.name }.className).apply {
-    info("Using $urlPrefix")
+): TypedHttpClient(urlPrefix, reqModifier, errorHandler, retryCount, retryAfter, maxLoggedLen, http, MimeTypes.json) {
+  override fun render(o: Any?) = if (o is String) o else json.stringify(o)
+
+  @Suppress("UNCHECKED_CAST")
+  override fun <T: Any> parse(body: String, type: KClass<T>): T = when (type) {
+    Unit::class -> Unit as T
+    String::class -> body as T
+    else -> json.parse(body, type)
   }
-
-  private fun jsonReq(urlSuffix: String) = HttpRequest.newBuilder().uri(URI("$urlPrefix$urlSuffix"))
-    .setHeader("Content-Type", "application/json; charset=UTF-8").setHeader("Accept", "application/json")
-    .timeout(10.seconds).reqModifier()
-
-  private suspend fun <T: Any> request(urlSuffix: String, type: KClass<T>, payload: String? = null, builder: RequestModifier): T {
-    val req = jsonReq(urlSuffix).builder().build()
-    val start = System.nanoTime()
-    val res = http.sendAsync(req, ofString()).await()
-    val ms = (System.nanoTime() - start) / 1000_000
-    val body = res.body().trim() // TODO: NPE -> return nullable type
-    if (res.statusCode() < 300) {
-      logger.info("${req.method()} $urlSuffix ${cut(payload)} in $ms ms: ${cut(body)}")
-      @Suppress("UNCHECKED_CAST") return when (type) {
-        Unit::class -> Unit as T
-        String::class -> body as T
-        else -> json.parse(body, type)
-      }
-    }
-    else {
-      logger.error("Failed ${req.method()} $urlSuffix ${cut(payload)} in $ms ms: ${res.statusCode()}: $body")
-      errorHandler(res, body)
-    }
-  }
-
-  private fun cut(s: String?) = if (s == null) "" else if (s.length <= maxLoggedLen) s else s.substring(0, maxLoggedLen) + "..."
-
-  suspend fun <T: Any> retryRequest(urlSuffix: String, type: KClass<T>, payload: String? = null, builder: RequestModifier): T {
-    for (i in 0..retryCount) {
-      try {
-        return request(urlSuffix, type, payload, builder)
-      } catch (e: IOException) {
-        if (i < retryCount) {
-          logger.error("Failed $urlSuffix, retry ${i + 1} after $retryAfter", e)
-          delay(retryAfter.inWholeMilliseconds)
-        }
-        else {
-          logger.error("Failed $urlSuffix: ${cut(payload)}", e)
-          throw e
-        }
-      }
-    }
-    error("Unreachable")
-  }
-
-  suspend inline fun <reified T: Any> request(urlSuffix: String, payload: String? = null, noinline builder: RequestModifier) = retryRequest(urlSuffix, T::class, payload, builder)
-
-  suspend fun <T: Any> get(urlSuffix: String, type: KClass<T>, modifier: RequestModifier? = null) = retryRequest(urlSuffix, type) { GET().apply(modifier) }
-  suspend inline fun <reified T: Any> get(urlSuffix: String, noinline modifier: RequestModifier? = null) = get(urlSuffix, T::class, modifier)
-
-  suspend fun <T: Any> post(urlSuffix: String, o: Any?, type: KClass<T>, modifier: RequestModifier? = null) = toJson(o).let { retryRequest(urlSuffix, type, it) { POST(ofString(it)).apply(modifier) } }
-  suspend inline fun <reified T: Any> post(urlSuffix: String, o: Any?, noinline modifier: RequestModifier? = null) = post(urlSuffix, o, T::class, modifier)
-
-  suspend fun <T: Any> put(urlSuffix: String, o: Any?, type: KClass<T>, modifier: RequestModifier? = null) = toJson(o).let { retryRequest(urlSuffix, type, it) { PUT(ofString(it)).apply(modifier) } }
-  suspend inline fun <reified T: Any> put(urlSuffix: String, o: Any?, noinline modifier: RequestModifier? = null) = put(urlSuffix, o, T::class, modifier)
-
-  suspend fun <T: Any> delete(urlSuffix: String, type: KClass<T>, modifier: RequestModifier? = null) = retryRequest(urlSuffix, type) { DELETE().apply(modifier) }
-  suspend inline fun <reified T: Any> delete(urlSuffix: String, noinline modifier: RequestModifier? = null) = delete(urlSuffix, T::class, modifier)
-
-  private fun toJson(o: Any?) = if (o is String) o else json.stringify(o)
-  private fun HttpRequest.Builder.apply(modifier: RequestModifier?) = modifier?.let { it() } ?: this
 }
-
-fun HttpClient.Builder.connectTimeout(duration: Duration): HttpClient.Builder = connectTimeout(duration.toJavaDuration())
-fun HttpRequest.Builder.timeout(duration: Duration): HttpRequest.Builder = timeout(duration.toJavaDuration())
---
+++ server/src/klite/http/TypedHttpClient.kt
@@ -0,0 +1,107 @@
+package klite.http
+
+import klite.error
+import klite.info
+import klite.logger
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.future.await
+import java.io.IOException
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import kotlin.reflect.KClass
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
+
+typealias RequestModifier =  HttpRequest.Builder.() -> HttpRequest.Builder
+
+/**
+ * Configure a default java.net.HttpClient in your registry with proper default timeout
+ */
+open class TypedHttpClient(
+  protected val urlPrefix: String = "",
+  val reqModifier: RequestModifier = { this },
+  val errorHandler: (HttpResponse<*>, String) -> Nothing = { res, body -> throw IOException("Failed with ${res.statusCode()}: $body") },
+  val retryCount: Int = 0,
+  val retryAfter: Duration = 1.seconds,
+  private val maxLoggedLen: Int = 1000,
+  val http: HttpClient,
+  val contentType: String
+) {
+  val logger = logger(Exception().stackTrace.first { it.className !== javaClass.name }.className).apply {
+    info("Using $urlPrefix")
+  }
+
+  private fun buildReq(urlSuffix: String) = HttpRequest.newBuilder().uri(URI("$urlPrefix$urlSuffix"))
+    .setHeader("Content-Type", "application/json; charset=UTF-8").setHeader("Accept", "application/json")
+    .timeout(10.seconds).reqModifier()
+
+  private suspend fun <T: Any> request(urlSuffix: String, type: KClass<T>, payload: String? = null, builder: RequestModifier): T {
+    val req = buildReq(urlSuffix).builder().build()
+    val start = System.nanoTime()
+    val res = http.sendAsync(req, HttpResponse.BodyHandlers.ofString()).await()
+    val ms = (System.nanoTime() - start) / 1000_000
+    val body = res.body().trim() // TODO: NPE -> return nullable type
+    if (res.statusCode() < 300) {
+      logger.info("${req.method()} $urlSuffix ${cut(payload)} in $ms ms: ${cut(body)}")
+      return parse(body, type)
+    }
+    else {
+      logger.error("Failed ${req.method()} $urlSuffix ${cut(payload)} in $ms ms: ${res.statusCode()}: $body")
+      errorHandler(res, body)
+    }
+  }
+
+  private fun cut(s: String?) = if (s == null) "" else if (s.length <= maxLoggedLen) s else s.substring(0, maxLoggedLen) + "..."
+
+  suspend fun <T: Any> retryRequest(urlSuffix: String, type: KClass<T>, payload: String? = null, builder: RequestModifier): T {
+    for (i in 0..retryCount) {
+      try {
+        return request(urlSuffix, type, payload, builder)
+      } catch (e: IOException) {
+        if (i < retryCount) {
+          logger.error("Failed $urlSuffix, retry ${i + 1} after $retryAfter", e)
+          delay(retryAfter.inWholeMilliseconds)
+        }
+        else {
+          logger.error("Failed $urlSuffix: ${cut(payload)}", e)
+          throw e
+        }
+      }
+    }
+    error("Unreachable")
+  }
+
+  suspend inline fun <reified T: Any> request(urlSuffix: String, payload: String? = null, noinline builder: RequestModifier) = retryRequest(urlSuffix, T::class, payload, builder)
+
+  suspend fun <T: Any> get(urlSuffix: String, type: KClass<T>, modifier: RequestModifier? = null) = retryRequest(urlSuffix, type) { GET().apply(modifier) }
+  suspend inline fun <reified T: Any> get(urlSuffix: String, noinline modifier: RequestModifier? = null) = get(urlSuffix, T::class, modifier)
+
+  suspend fun <T: Any> post(urlSuffix: String, o: Any?, type: KClass<T>, modifier: RequestModifier? = null) = render(o).let { retryRequest(urlSuffix, type, it) { POST(
+    HttpRequest.BodyPublishers.ofString(it)
+  ).apply(modifier) } }
+  suspend inline fun <reified T: Any> post(urlSuffix: String, o: Any?, noinline modifier: RequestModifier? = null) = post(urlSuffix, o, T::class, modifier)
+
+  suspend fun <T: Any> put(urlSuffix: String, o: Any?, type: KClass<T>, modifier: RequestModifier? = null) = render(o).let { retryRequest(urlSuffix, type, it) { PUT(
+    HttpRequest.BodyPublishers.ofString(it)
+  ).apply(modifier) } }
+  suspend inline fun <reified T: Any> put(urlSuffix: String, o: Any?, noinline modifier: RequestModifier? = null) = put(urlSuffix, o, T::class, modifier)
+
+  suspend fun <T: Any> delete(urlSuffix: String, type: KClass<T>, modifier: RequestModifier? = null) = retryRequest(urlSuffix, type) { DELETE().apply(modifier) }
+  suspend inline fun <reified T: Any> delete(urlSuffix: String, noinline modifier: RequestModifier? = null) = delete(urlSuffix, T::class, modifier)
+
+  private fun HttpRequest.Builder.apply(modifier: RequestModifier?) = modifier?.let { it() } ?: this
+
+  protected open fun render(o: Any?) = o.toString()
+
+  @Suppress("UNCHECKED_CAST")
+  protected open fun <T: Any> parse(body: String, type: KClass<T>) = when (type) {
+    Unit::class -> Unit as T
+    else -> body as T
+  }
+}
+
+fun HttpClient.Builder.connectTimeout(duration: Duration): HttpClient.Builder = connectTimeout(duration.toJavaDuration())
+fun HttpRequest.Builder.timeout(duration: Duration): HttpRequest.Builder = timeout(duration.toJavaDuration())
