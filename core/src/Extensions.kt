053dce8e59c8c5f95a2fb7d42aeddd341bf07d42
Anton Keks <anton@codeborne.com>
2023-02-25 18:39:47 +0200

    add notNullValues() function that can be used with jdbc where

--- core/src/Extensions.kt
+++ core/src/Extensions.kt
@@ -6,7 +6,8 @@ import kotlin.reflect.KType
 fun String?.trimToNull() = this?.trim()?.takeIf { it.isNotEmpty() }
 
 @Suppress("UNCHECKED_CAST")
-fun <K, V> mapOfNotNull(vararg pairs: Pair<K, V?>) = mapOf(*pairs).filterValues { it != null } as Map<K, V>
+fun <K, V> notNullValues(vararg pairs: Pair<K, V?>?) = pairs.filter { it?.second != null } as List<Pair<K, V>>
+fun <K, V> mapOfNotNull(vararg pairs: Pair<K, V?>?) = notNullValues(*pairs).toMap()
 
 val KType.java get() = (classifier as KClass<*>).java
 fun Any.unboxInline() = javaClass.getMethod("unbox-impl").invoke(this)
