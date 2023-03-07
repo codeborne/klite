aca78981546869af7804014e1039a525c108551f
Anton Keks <anton@codeborne.com>
2023-03-07 22:57:50 +0200

    add JUnitAssertionImprover to improve mockk/atrium assertion messages

---
+++ jdbc-test/src/JUnitAssertionImprover.kt
@@ -0,0 +1,23 @@
+package klite.jdbc
+
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.api.extension.InvocationInterceptor
+import org.junit.jupiter.api.extension.ReflectiveInvocationContext
+import java.lang.reflect.Method
+
+/** Improves mockk & atrium assertion error messages by locating expected and actual values one below another */
+class JUnitAssertionImprover: InvocationInterceptor {
+  override fun interceptTestMethod(invocation: InvocationInterceptor.Invocation<Void>, invocationContext: ReflectiveInvocationContext<Method>, extensionContext: ExtensionContext) {
+    try {
+      super.interceptTestMethod(invocation, invocationContext, extensionContext)
+    } catch (e: AssertionError) {
+      try {
+        Throwable::class.java.getDeclaredField("detailMessage").apply {
+          isAccessible = true // needs --add-opens=java.base/java.lang=ALL-UNNAMED
+          set(e, e.message?.replace(", matcher:", "\n   matcher:")?.replace("◆ to equal:", "        ◆ to equal:"))
+        }
+      } catch (ignore: Exception) { }
+      throw e
+    }
+  }
+}
