e00c126c75c94c61d82d746edf719123d61f4d02
Anton Keks <anton@codeborne.com>
2023-03-10 16:42:51 +0200

    add more generic get() and getOrNull()

--- json/src/JsonNode.kt
+++ json/src/JsonNode.kt
@@ -1,13 +1,16 @@
-@file:Suppress("UNCHECKED_CAST")
+@file:Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE")
 package klite.json
 
 typealias JsonNode = Map<String, Any?>
 typealias JsonList = List<JsonNode>
 
-fun JsonNode.getString(key: String) = get(key) as String
-fun JsonNode.getInt(key: String) = (get(key) as Number).toInt()
-fun JsonNode.getLong(key: String) = (get(key) as Number).toLong()
-fun JsonNode.getBigDecimal(key: String) = get(key)!!.toString().toBigDecimal()
-fun <T> JsonNode.getList(key: String) = get(key) as List<T>
-fun <T> JsonNode.getMap(key: String) = get(key) as Map<String, T>
-fun JsonNode.getNode(key: String): JsonNode = getMap(key)
+inline fun <T> JsonNode.get(key: String) = get(key) as T
+inline fun <T> JsonNode.getOrNull(key: String) = get(key) as T?
+
+inline fun JsonNode.getString(key: String) = get<String>(key)
+inline fun JsonNode.getInt(key: String) = get<Number>(key).toInt()
+inline fun JsonNode.getLong(key: String) = get<Number>(key).toLong()
+inline fun JsonNode.getBigDecimal(key: String) = get<String>(key).toBigDecimal()
+inline fun <T> JsonNode.getList(key: String) = get<List<T>>(key)
+inline fun <T> JsonNode.getMap(key: String) = get<Map<String, T>>(key)
+inline fun JsonNode.getNode(key: String): JsonNode = getMap(key)
