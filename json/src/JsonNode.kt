08de847cb2a1f1b37eadec96b82fbb47aba0a63a
Anton Keks <anton@codeborne.com>
2023-03-10 17:28:17 +0200

    make new get() hide the original from Map

--- json/src/JsonNode.kt
+++ json/src/JsonNode.kt
@@ -1,9 +1,10 @@
-@file:Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE")
+@file:Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE", "INVISIBLE_REFERENCE", "INVISIBLE_MEMBER")
 package klite.json
 
 typealias JsonNode = Map<String, Any?>
 typealias JsonList = List<JsonNode>
 
+@kotlin.internal.HidesMembers
 inline fun <T> JsonNode.get(key: String) = get(key) as T
 inline fun <T> JsonNode.getOrNull(key: String) = get(key) as T?
 
@@ -11,6 +12,6 @@ inline fun JsonNode.getString(key: String) = get<String>(key)
 inline fun JsonNode.getInt(key: String) = get<Number>(key).toInt()
 inline fun JsonNode.getLong(key: String) = get<Number>(key).toLong()
 inline fun JsonNode.getBigDecimal(key: String) = get<String>(key).toBigDecimal()
-inline fun <T> JsonNode.getList(key: String) = get<List<T>>(key)
+inline fun <T> JsonNode.getList(key: String) = get<List<T>>(key).forEach {  }
 inline fun <T> JsonNode.getMap(key: String) = get<Map<String, T>>(key)
 inline fun JsonNode.getNode(key: String): JsonNode = getMap(key)
