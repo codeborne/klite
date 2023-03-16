58348452bc6862693de1cd4a1b18ce3a039687a3
Anton Keks <anton@codeborne.com>
2023-03-16 22:53:37 +0200

    make TSGenerator open for overriding possibility

--- json/src/TSGenerator.kt
+++ json/src/TSGenerator.kt
@@ -14,17 +14,20 @@ import kotlin.reflect.KType
 import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.memberProperties
 
-/** Converts project data classes/enums to TypeScript for front-end type-safety */
-class TSGenerator(
+/** Converts project data/enum/inline classes to TypeScript for front-end type-safety */
+open class TSGenerator(
   private val customTypes: Map<String, String> = emptyMap(),
   private val typePrefix: String = "export "
 ) {
   @OptIn(ExperimentalPathApi::class)
-  fun scan(dir: Path) {
+  open fun scan(dir: Path) {
     dir.walk(INCLUDE_DIRECTORIES).filter { it.extension == "class" }.forEach {
       try {
         val cls = Class.forName(dir.relativize(it).toString().removeSuffix(".class").replace("/", ".")).kotlin
-        render(cls)?.let { println(typePrefix + it) }
+        render(cls)?.let {
+          println("// $cls")
+          println(typePrefix + it)
+        }
       } catch (ignore: UnsupportedOperationException) {
       } catch (e: ClassNotFoundException) {
         err.println("// $e")
@@ -32,19 +35,19 @@ class TSGenerator(
     }
   }
 
-  @Language("TypeScript") fun render(cls: KClass<*>) =
+  @Language("TypeScript") open fun render(cls: KClass<*>) =
     if (cls.isData || cls.java.isInterface && !cls.java.isAnnotation) renderInterface(cls)
     else if (cls.isValue) renderInline(cls)
     else if (cls.isSubclassOf(Enum::class)) renderEnum(cls)
     else null
 
-  private fun renderEnum(cls: KClass<*>) = "enum " + tsName(cls) + " {" + cls.java.enumConstants.joinToString { "$it = '$it'" } + "}"
+  protected open fun renderEnum(cls: KClass<*>) = "enum " + tsName(cls) + " {" + cls.java.enumConstants.joinToString { "$it = '$it'" } + "}"
 
-  private fun renderInline(cls: KClass<*>) = "type " + tsName(cls) +
+  protected open fun renderInline(cls: KClass<*>) = "type " + tsName(cls) +
     (cls.typeParameters.takeIf { it.isNotEmpty() }?.joinToString(prefix = "<", postfix = ">") ?: "") +
     " = " + tsType(cls.memberProperties.first().returnType)
 
-  private fun renderInterface(cls: KClass<*>): String {
+  protected open fun renderInterface(cls: KClass<*>): String {
     val sb = StringBuilder()
     sb.append("interface ").append(tsName(cls)).append(" {")
     (cls.publicProperties as Sequence<KProperty1<Any, *>>).notIgnored.forEach { p ->
@@ -56,7 +59,7 @@ class TSGenerator(
     return sb.append("}").toString()
   }
 
-  private fun tsType(type: KType): String {
+  protected open fun tsType(type: KType): String {
     val cls = type.classifier as KClass<*>
     return customTypes[type.toString()] ?: (when {
       cls.isValue -> tsName(cls)
@@ -71,7 +74,7 @@ class TSGenerator(
     } + (type.arguments.takeIf { it.isNotEmpty() }?.joinToString(prefix = "<", postfix = ">") { tsType(it.type!!) } ?: ""))
   }
 
-  private fun tsName(type: KClass<*>) = type.java.name.substringAfterLast(".").replace("$", "")
+  protected open fun tsName(type: KClass<*>) = type.java.name.substringAfterLast(".").replace("$", "")
 }
 
 fun main(args: Array<String>) {
