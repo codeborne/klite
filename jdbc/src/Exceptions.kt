7a2d4de35adc0a8a2367ed024129bd940c7a8ab7
Anton Keks, Vadim Vohmjanin <anton@codeborne.com, vadim@codeborne.com>
2023-03-22 16:14:16 +0200

    show PSQLException detail if it's available

--- jdbc/src/Exceptions.kt
+++ jdbc/src/Exceptions.kt
@@ -2,4 +2,7 @@ package klite.jdbc
 
 import klite.BusinessException
 
-class AlreadyExistsException(cause: Throwable? = null): BusinessException("errors.alreadyExists", cause)
+class AlreadyExistsException(cause: Throwable? = null): BusinessException(cause?.message?.let {
+  val pos = it.indexOf("Detail: ")
+  if (pos >= 0) it.substring(pos + 8) else null
+} ?: "errors.alreadyExists", cause)
