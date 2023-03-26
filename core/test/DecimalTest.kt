f594f7159f5069c3369d3dc1dc208c2c30c5137a
Anton Keks <anton@codeborne.com>
2023-03-26 20:51:44 +0300

    split Decimal tests

--- core/test/DecimalTest.kt
+++ core/test/DecimalTest.kt
@@ -5,11 +5,20 @@ import klite.d
 import org.junit.jupiter.api.Test
 
 class DecimalTest {
-  @Test fun `equals & hashCode`() {
+  @Test fun equals() {
     expect("1.00".d == 1.d).toEqual(true)
-    expect("1.00".d.hashCode()).toEqual(1.d.hashCode())
-
     expect("1.01".d != 1.d).toEqual(true)
+    expect("1e3".d == 1000.d).toEqual(true)
+  }
+
+  @Test fun hashCodes() {
+    expect("1.00".d.hashCode()).toEqual(1.d.hashCode())
     expect("1.01".d.hashCode()).notToEqual(1.d.hashCode())
+  }
+
+  @Test fun strings() {
+    expect("3.00".d.toString()).toEqual("3.00")
+    expect(3.d.toString()).toEqual("3")
+    expect("1e3".d.toString()).toEqual("1000")
   }
 }
