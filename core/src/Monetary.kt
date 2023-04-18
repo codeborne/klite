607a4f67d2cd641c874ab063ad64195bddc21f30
Anton Keks, Vadim Vohmjanin <anton@codeborne.com, vadim@codeborne.com>
2023-04-18 15:27:00 +0300

    rename internal field to something resembling cents

--- core/src/Monetary.kt
+++ core/src/Monetary.kt
@@ -1,9 +1,8 @@
 package klite
 
-import kotlin.math.roundToInt
 import kotlin.math.roundToLong
 
-internal class Monetary private constructor(private val v: Long): Comparable<Monetary>, Number() {
+internal class Monetary private constructor(private val c: Long): Comparable<Monetary>, Number() {
   companion object {
     const val DECIMALS = 100L
     val MAX_VALUE = Monetary(Long.MAX_VALUE / DECIMALS)
@@ -16,26 +15,26 @@ internal class Monetary private constructor(private val v: Long): Comparable<Mon
   constructor(v: Float): this((v * DECIMALS).roundToLong())
   constructor(v: String): this(v.split('.').let { it[0].toLong() * DECIMALS + (it.getOrNull(1)?.padEnd(2, '0')?.toLong() ?: 0) })
 
-  operator fun unaryMinus() = Monetary(-v)
+  operator fun unaryMinus() = Monetary(-c)
   operator fun unaryPlus() = this
 
-  operator fun plus(o: Monetary) = Monetary(Math.addExact(v, o.v))
-  operator fun minus(o: Monetary) = Monetary(Math.subtractExact(v, o.v))
-  operator fun times(o: Monetary) = Monetary(Math.multiplyExact(v, o.v) / DECIMALS)
+  operator fun plus(o: Monetary) = Monetary(Math.addExact(c, o.c))
+  operator fun minus(o: Monetary) = Monetary(Math.subtractExact(c, o.c))
+  operator fun times(o: Monetary) = Monetary(Math.multiplyExact(c, o.c) / DECIMALS)
   operator fun div(o: Monetary) = Monetary(toDouble() / o.toDouble())
 
-  operator fun times(o: Long) = Monetary(Math.multiplyExact(v, o))
-  operator fun div(o: Long) = Monetary(v / o)
+  operator fun times(o: Long) = Monetary(Math.multiplyExact(c, o))
+  operator fun div(o: Long) = Monetary(c / o)
 
-  override fun equals(o: Any?) = v == (o as? Monetary)?.v
-  override fun hashCode() = v.hashCode()
-  override fun compareTo(o: Monetary) = v.compareTo(o.v)
+  override fun equals(o: Any?) = c == (o as? Monetary)?.c
+  override fun hashCode() = c.hashCode()
+  override fun compareTo(o: Monetary) = c.compareTo(o.c)
 
-  override fun toString() = "${v / DECIMALS}.${(v % DECIMALS).toString().padStart(2, '0')}"
+  override fun toString() = "${c / DECIMALS}.${(c % DECIMALS).toString().padStart(2, '0')}"
 
-  override fun toDouble() = v / DECIMALS.toDouble()
-  override fun toFloat() = v / DECIMALS.toFloat()
-  override fun toLong() = v / DECIMALS
+  override fun toDouble() = c / DECIMALS.toDouble()
+  override fun toFloat() = c / DECIMALS.toFloat()
+  override fun toLong() = c / DECIMALS
   override fun toInt() = toLong().toInt()
   override fun toShort() = toLong().toShort()
   override fun toByte() = toLong().toByte()
