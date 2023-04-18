50fd3956111a2d3238d3319f67d673a5ef3beabc
Anton Keks, Vadim Vohmjanin <anton@codeborne.com, vadim@codeborne.com>
2023-04-18 16:03:12 +0300

    implement some conversions outside of class constructors

--- core/src/Decimal.kt
+++ core/src/Decimal.kt
@@ -1,35 +1,34 @@
 package klite
 
+import klite.Decimal.Companion.DECIMALS
+import java.lang.Math.*
 import kotlin.math.absoluteValue
 import kotlin.math.roundToLong
 
 /** A monetary amount with 2 fraction digits, lighter than BigDecimal and having numeric equals */
-class Decimal private constructor(private val c: Long): Comparable<Decimal>, Number() {
+class Decimal internal constructor(private val c: Long): Comparable<Decimal>, Number() {
   companion object {
     const val DECIMALS = 100L
     val MAX_VALUE = Decimal(Long.MAX_VALUE)
     val MIN_VALUE = Decimal(Long.MIN_VALUE)
   }
 
-  constructor(v: Int): this(v * DECIMALS)
-  // TODO: Long
   constructor(v: Double): this((v * DECIMALS).roundToLong())
-  constructor(v: Float): this((v * DECIMALS).roundToLong())
   constructor(v: String): this(v.split('.').let { it[0].toLong() * DECIMALS + (it.getOrNull(1)?.padEnd(2, '0')?.toLong() ?: 0) })
 
   operator fun unaryMinus() = Decimal(-c)
   operator fun unaryPlus() = this
 
-  operator fun plus(o: Decimal) = Decimal(Math.addExact(c, o.c))
-  operator fun minus(o: Decimal) = Decimal(Math.subtractExact(c, o.c))
-  operator fun times(o: Decimal) = Decimal(Math.multiplyExact(c, o.c) / DECIMALS)
+  operator fun plus(o: Decimal) = Decimal(addExact(c, o.c))
+  operator fun minus(o: Decimal) = Decimal(subtractExact(c, o.c))
+  operator fun times(o: Decimal) = Decimal(multiplyExact(c, o.c) / DECIMALS)
   operator fun div(o: Decimal) = Decimal(toDouble() / o.toDouble())
   operator fun rem(o: Decimal) = Decimal(c % o.c)
 
   operator fun inc() = plus(1.d)
   operator fun dec() = minus(1.d)
 
-  operator fun times(o: Long) = Decimal(Math.multiplyExact(c, o))
+  operator fun times(o: Long) = Decimal(multiplyExact(c, o))
   operator fun div(o: Long) = Decimal(c / o)
 
   infix fun percent(p: Decimal) = times(p) / DECIMALS
@@ -49,7 +48,8 @@ class Decimal private constructor(private val c: Long): Comparable<Decimal>, Num
   override fun toChar() = toInt().toChar()
 }
 
-val Int.d get() = Decimal(this)
+val Int.d get() = Decimal(multiplyExact(DECIMALS, this))
+val Long.d get() = Decimal(multiplyExact(DECIMALS, this))
 val Double.d get() = Decimal(this)
-val Float.d get() = Decimal(this)
+val Float.d get() = Decimal(toDouble())
 val String.d get() = Decimal(this)
