a57760276066bb85d74b36c0a95a1c95857a0015
Anton Keks, Vadim Vohmjanin <anton@codeborne.com, vadim@codeborne.com>
2023-04-18 17:23:09 +0300

    add JsonNode.getDecimal() for convenience

--- json/src/JsonNode.kt
+++ json/src/JsonNode.kt
@@ -1,6 +1,8 @@
 @file:Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE", "INVISIBLE_REFERENCE", "INVISIBLE_MEMBER")
 package klite.json
 
+import klite.d
+
 typealias JsonNode = Map<String, Any?>
 typealias JsonList = List<JsonNode>
 
@@ -12,6 +14,7 @@ inline fun JsonNode.getString(key: String) = get<String>(key)
 inline fun JsonNode.getInt(key: String) = get<Number>(key).toInt()
 inline fun JsonNode.getLong(key: String) = get<Number>(key).toLong()
 inline fun JsonNode.getBigDecimal(key: String) = get<String>(key).toBigDecimal()
+inline fun JsonNode.getDecimal(key: String) = get<String>(key).d
 inline fun <T> JsonNode.getList(key: String) = get<List<T>>(key).forEach {  }
 inline fun <T> JsonNode.getMap(key: String) = get<Map<String, T>>(key)
 inline fun JsonNode.getNode(key: String): JsonNode = getMap(key)
