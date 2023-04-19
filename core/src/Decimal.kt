5782c8de7bd3d737cc2a17a26ff625c405add9a7
Anton Keks <anton@codeborne.com>
2023-04-19 21:32:46 +0300

    rename constant

--- core/src/Decimal.kt
+++ core/src/Decimal.kt
@@ -1,6 +1,6 @@
 package klite
 
-import klite.Decimal.Companion.DECIMALS
+import klite.Decimal.Companion.CENTS
 import java.lang.Math.*
 import kotlin.math.absoluteValue
 import kotlin.math.roundToLong
@@ -8,14 +8,14 @@ import kotlin.math.roundToLong
 /** A monetary amount with 2 fraction digits, lighter than BigDecimal and having numeric equals */
 class Decimal internal constructor(private val c: Long): Comparable<Decimal>, Number() {
   companion object {
-    const val DECIMALS = 100L
+    const val CENTS = 100L
     @JvmStatic val MAX_VALUE = Decimal(Long.MAX_VALUE)
     @JvmStatic val MIN_VALUE = Decimal(Long.MIN_VALUE)
     @JvmStatic val ZERO = Decimal(0)
-    @JvmStatic val ONE = Decimal(DECIMALS)
+    @JvmStatic val ONE = Decimal(CENTS)
   }
 
-  constructor(v: Double): this((v * DECIMALS).roundToLong())
+  constructor(v: Double): this((v * CENTS).roundToLong())
   constructor(v: String): this(v.toDouble())
 
   operator fun unaryMinus() = Decimal(-c)
@@ -23,7 +23,7 @@ class Decimal internal constructor(private val c: Long): Comparable<Decimal>, Nu
 
   operator fun plus(o: Decimal) = Decimal(addExact(c, o.c))
   operator fun minus(o: Decimal) = Decimal(subtractExact(c, o.c))
-  operator fun times(o: Decimal) = Decimal(multiplyExact(c, o.c) / DECIMALS)
+  operator fun times(o: Decimal) = Decimal(multiplyExact(c, o.c) / CENTS)
   operator fun div(o: Decimal) = Decimal(toDouble() / o.toDouble())
   operator fun rem(o: Decimal) = Decimal(c % o.c)
 
@@ -38,25 +38,25 @@ class Decimal internal constructor(private val c: Long): Comparable<Decimal>, Nu
   operator fun dec() = minus(1.d)
 
   fun abs() = if (c < 0) Decimal(-c) else this
-  infix fun percent(p: Decimal) = times(p) / DECIMALS
+  infix fun percent(p: Decimal) = times(p) / CENTS
 
   override fun equals(o: Any?) = c == (o as? Decimal)?.c
   override fun hashCode() = c.hashCode()
   override fun compareTo(o: Decimal) = c.compareTo(o.c)
 
-  override fun toString() = "${c / DECIMALS}.${(c % DECIMALS).absoluteValue.toString().padStart(2, '0')}"
+  override fun toString() = "${c / CENTS}.${(c % CENTS).absoluteValue.toString().padStart(2, '0')}"
 
-  override fun toDouble() = c / DECIMALS.toDouble()
-  override fun toFloat() = c / DECIMALS.toFloat()
-  override fun toLong() = c / DECIMALS
+  override fun toDouble() = c / CENTS.toDouble()
+  override fun toFloat() = c / CENTS.toFloat()
+  override fun toLong() = c / CENTS
   override fun toInt() = toLong().toInt()
   override fun toShort() = toLong().toShort()
   override fun toByte() = toLong().toByte()
   override fun toChar() = toInt().toChar()
 }
 
-val Int.d get() = Decimal(multiplyExact(DECIMALS, this))
-val Long.d get() = Decimal(multiplyExact(DECIMALS, this))
+val Int.d get() = Decimal(multiplyExact(CENTS, this))
+val Long.d get() = Decimal(multiplyExact(CENTS, this))
 val Double.d get() = Decimal(this)
 val Float.d get() = Decimal(toDouble())
 val String.d get() = Decimal(this)
