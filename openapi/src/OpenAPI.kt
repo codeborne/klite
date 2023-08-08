08654652a976227a56c6c2faba91932ff1ace77d
Anton Keks <anton@codeborne.com>
2023-08-08 13:56:19 +0300

    filter out the request body param

--- openapi/src/OpenAPI.kt
+++ openapi/src/OpenAPI.kt
@@ -7,9 +7,12 @@ import klite.*
 import klite.StatusCode.Companion.OK
 import klite.annotations.*
 import java.math.BigDecimal
+import kotlin.reflect.KClass
 import kotlin.reflect.KClassifier
+import kotlin.reflect.KParameter
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.jvm.jvmName
 
 // Spec: https://swagger.io/specification/
 // Sample: https://github.com/OAI/OpenAPI-Specification/blob/main/examples/v3.0/api-with-examples.json
@@ -35,17 +38,17 @@ fun Router.openApi(path: String = "/openapi", title: String = "API", version: St
           val op = route.annotation<Operation>()
           (op?.method?.trimToNull() ?: route.method.name).lowercase() to mapOf(
             "operationId" to route.handler.let { (if (it is FunHandler) it.instance::class.simpleName + "." + it.f.name else it::class.simpleName) },
-            "parameters" to (route.handler as? FunHandler)?.let { it.params.map { p -> mapOf(
+            "parameters" to (route.handler as? FunHandler)?.let { it.params.filter { it.source != null }.map { p -> mapOf(
               "name" to p.name,
               "required" to (!p.p.isOptional && !p.p.type.isMarkedNullable),
               "in" to toParameterIn(p.source),
               "schema" to toSchema(p.p.type.classifier),
             ) + ((p.p.findAnnotation<Parameter>() ?: op?.parameters?.find { it.name == p.name })?.toNonEmptyValues() ?: emptyMap()) } },
+            "requestBody" to (route.handler as? FunHandler)?.params?.find { it.source == null && !(it.p.type.classifier as KClass<*>).jvmName.startsWith("klite") }?.p?.toRequestBody(),
             "responses" to mapOf(
               OK.value to mapOf("description" to "OK")
             )
           ) + (op?.let { it.toNonEmptyValues { it.name != "method" } + mapOf(
-            // TODO: describe body parameter from the route method
             "requestBody" to op.requestBody.toNonEmptyValues().takeIf { it.isNotEmpty() },
             "externalDocs" to op.externalDocs.toNonEmptyValues().takeIf { it.isNotEmpty() }
           ) } ?: emptyMap())
@@ -69,6 +72,8 @@ private fun toSchema(type: KClassifier?) = mapOf("type" to when(type) {
   BigDecimal::class, Decimal::class, Float::class, Double::class -> "float" // TODO: is this correct
   else -> "string"
 }) // TODO: enum, object
+
+private fun KParameter.toRequestBody() = mapOf("content" to mapOf("schema" to toSchema(type.classifier)))
 
 private fun <T: Annotation> T.toNonEmptyValues(filter: (KProperty1<T, *>) -> Boolean = {true}) = toValues(publicProperties.filter(filter)).filterValues {
   it != "" && it != false && (it as? Array<*>)?.isEmpty() != true
