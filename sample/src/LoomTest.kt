3bece00529f010575fc73fce0a5da1c3f0f6c526
Anton Keks <anton@codeborne.com>
2023-08-25 13:39:02 +0300

    test concurrency with sync methods of HttpClient

--- sample/src/LoomTest.kt
+++ sample/src/LoomTest.kt
@@ -10,16 +10,21 @@ import java.util.concurrent.Executors
 fun main() {
   Config.useEnvFile()
 //  val db = PooledDataSource(maxSize = 100)
-  val url = URI("https://www.google.com/")
+  val url = URI("http://localhost:8080/wait")
   val httpClient = HttpClient.newHttpClient()
   HttpServer.create(InetSocketAddress(8080), 10).apply {
     executor = Executors.newThreadPerTaskExecutor(Thread.ofVirtual().name("req-", 1).factory())
+    createContext("/wait") { e ->
+      Thread.sleep(1000)
+      e.sendResponseHeaders(200, 2)
+      e.responseBody.writer().use { it.write("OK") }
+    }
     createContext("/hello") { e ->
 //      val dbTime = db.query("select pg_sleep(1), clock_timestamp()") { getString(2) }.first()
 //      val body = url.openStream().use { it.readAllBytes() }
       val body = httpClient.send(HttpRequest.newBuilder(url).GET().build(), BodyHandlers.ofString()).body()
       e.sendResponseHeaders(200, 0)
-      e.responseBody.writer().use { it.write("${body.length}, ${Thread.currentThread().name}\n") }
+      e.responseBody.writer().use { it.write(body) }
     }
     start()
   }
