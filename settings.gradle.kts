be2154602e8a5d87442f38146d41e3876aa53f13
Andrei Solntsev, Margo Mitt <andrei@codeborne.com, margo@codeborne.com>
2024-08-15 11:23:58 +0300

    bump junit from 5.10.1 to 5.11.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,18 +11,18 @@ dependencyResolutionManagement {
       library("kotlinx-serialization-json", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
       library("kotlinx-datetime", "org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
 
-      val junit5 = version("junit", "5.10.1")
+      val junit5 = version("junit", "5.11.0")
       library("junit", "org.junit.jupiter", "junit-jupiter").versionRef(junit5)
       library("junit-api", "org.junit.jupiter", "junit-jupiter-api").versionRef(junit5)
       library("junit-engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef(junit5)
       library("atrium", "ch.tutteli.atrium:atrium-fluent:1.2.0")
       library("mockk", "io.mockk:mockk:1.13.12")
 
-      val slf4j = version("slf4j", "2.0.9")
+      val slf4j = version("slf4j", "2.0.16")
       library("slf4j-api", "org.slf4j", "slf4j-api").versionRef(slf4j)
       library("slf4j-jul", "org.slf4j", "jul-to-slf4j").versionRef(slf4j)
 
-      val jackson = version("jackson", "2.16.0")
+      val jackson = version("jackson", "2.17.2")
       library("jackson-jsr310", "com.fasterxml.jackson.datatype", "jackson-datatype-jsr310").versionRef(jackson)
       library("jackson-kotlin", "com.fasterxml.jackson.module", "jackson-module-kotlin").versionRef(jackson)
 
