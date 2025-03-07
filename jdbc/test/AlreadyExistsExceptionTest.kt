7c428b50627936cf57a1e9312560ed9889d4a3b4
Anton Keks <anton@azib.net>
2025-03-07 16:10:14 +0200

    update test to how AlreadyExistsException now works

--- jdbc/test/AlreadyExistsExceptionTest.kt
+++ jdbc/test/AlreadyExistsExceptionTest.kt
@@ -2,7 +2,6 @@ package klite.jdbc
 
 import ch.tutteli.atrium.api.fluent.en_GB.toEqual
 import ch.tutteli.atrium.api.verbs.expect
-import klite.jdbc.AlreadyExistsException
 import org.junit.jupiter.api.Test
 import java.sql.SQLException
 
@@ -10,7 +9,7 @@ class AlreadyExistsExceptionTest {
   @Test fun `take detail from Postgres exception`() {
     val e = SQLException("ERROR: duplicate key value violates unique constraint \"people_personalcode_key\"\n" +
       "  Detail: Key (personalcode)=(38202032213) already exists.")
-    expect(AlreadyExistsException(e).message).toEqual("Key (personalcode)=(38202032213) already exists.")
+    expect(AlreadyExistsException(e).message).toEqual("errors.alreadyExists: personalcode=38202032213")
   }
 
   @Test fun `no detail in SQL exception`() {
