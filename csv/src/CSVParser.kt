8941787cccabd542bdbda590a5d9df5636b957b5
Anton Keks <anton@azib.net>
2025-03-07 16:03:56 +0200

    switch back to UTF-8 if BOM was provided

--- csv/src/CSVParser.kt
+++ csv/src/CSVParser.kt
@@ -8,13 +8,24 @@ class CSVParser(separator: String = ",", private val charset: Charset = UTF_8) {
   private val splitter = """(?:$separator|^)("((?:(?:"")*[^"]*)*)"|([^"$separator]*))""".toRegex()
 
   fun parse(stream: InputStream): Sequence<Map<String, String>> {
-    val lines = stream.bufferedReader(charset).lineSequence().iterator()
+    val lines = stream.bufferedReader(skipBOM(stream)).lineSequence().iterator()
     var headerLine = lines.next()
     if (headerLine.startsWith(bomUTF8)) headerLine = headerLine.substring(bomUTF8.length)
     val header = splitLine(headerLine).toList()
     return lines.asSequence().map {
       splitLine(it).withIndex().associate { header[it.index] to it.value }
     }
+  }
+
+  private fun skipBOM(stream: InputStream): Charset {
+    if (stream.markSupported()) {
+      stream.mark(2)
+      stream.readNBytes(2).let {
+        if (it.contentEquals(bomUTF8.toByteArray())) return UTF_8
+        else stream.reset()
+      }
+    }
+    return charset
   }
 
   internal fun splitLine(line: String) = splitter.findAll(line).map {
