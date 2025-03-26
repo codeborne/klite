6d2b6d308806ccbee581b853f7bbbf0160171d7e
Anton Keks <anton@azib.net>
2025-03-26 22:35:30 +0200

    update kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "klite"
 dependencyResolutionManagement {
   versionCatalogs {
     create("libs") {
-      version("kotlin", "2.1.10")
+      version("kotlin", "2.1.20")
 
       val coroutines = version("coroutines", "1.9.0")
       library("kotlinx-coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-jdk8").versionRef(coroutines)
